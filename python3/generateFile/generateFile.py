4ca9339290f30fe9f8deadbdce299c7da2578f54
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2017-11-06 22:05:42 +0200

    upd

---
+++ python3/generateFile/generateFile.py
@@ -0,0 +1,40 @@
+start = """<?xml version="1.0"?>
+<catalog>
+"""
+
+s = """    <book id="bk101">
+      <author>Gambardella, Matthew</author>
+      <title>XML Developer's Guide</title>
+      <genre>Computer</genre>
+      <price>44.95</price>
+      <publish_date>2000-10-01</publish_date>
+      <description>An in-depth look at creating applications</description>
+      <subTrees>
+         <subTree>
+            <name>10n</name>
+            <value>10v</value>
+         </subTree>
+         <subTree>
+            <name>1010n</name>
+            <value>1010v</value>
+         </subTree>
+      </subTrees>
+    </book>
+"""
+
+end = """ </catalog>"""
+
+# print(start)
+# for x in range(0, 3):
+#     print(s)
+# print(end)
+
+fo = open("book_test_003_003.xml", "w")
+
+fo.write(start)
+for x in range(0, 200000):
+    fo.write(s)
+fo.write(end)
+
+# Close opend file
+fo.close()
