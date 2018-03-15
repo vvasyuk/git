b6d338fb361ebd82d90f0e5bdff5dd30a3355d83
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2018-03-15 22:39:35 +0200

    upd

--- java/igniteClient/src/main/java/com/ignite/config/ClientConfig.java
+++ java/igniteClient/src/main/java/com/ignite/config/ClientConfig.java
@@ -2,10 +2,7 @@ package com.ignite.config;
 
 import com.ignite.domain.Book;
 import com.ignite.domain.Library;
-import org.apache.ignite.DataRegionMetrics;
-import org.apache.ignite.Ignite;
-import org.apache.ignite.IgniteCache;
-import org.apache.ignite.Ignition;
+import org.apache.ignite.*;
 import org.apache.ignite.binary.BinaryBasicIdMapper;
 import org.apache.ignite.binary.BinaryBasicNameMapper;
 import org.apache.ignite.cache.CachePeekMode;
@@ -15,20 +12,24 @@ import org.apache.ignite.cache.query.ScanQuery;
 import org.apache.ignite.configuration.BinaryConfiguration;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.lang.IgniteBiInClosure;
 import org.apache.ignite.lang.IgniteBiPredicate;
 import org.apache.ignite.lang.IgniteCallable;
 import org.apache.ignite.resources.IgniteInstanceResource;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
+import org.apache.ignite.stream.StreamVisitor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
+import org.springframework.shell.standard.ShellMethod;
 
 import javax.cache.Cache;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Map;
 import java.util.Objects;
 
 
@@ -56,65 +57,9 @@ public class ClientConfig {
         cfg.setClientMode(true);
         cfg.setDiscoverySpi(spi);
 
-//        BinaryConfiguration bCfg = new BinaryConfiguration();
-//        bCfg.setNameMapper(new BinaryBasicNameMapper(false));
-//        bCfg.setIdMapper(new BinaryBasicIdMapper(false));
-//        bCfg.setClassNames(Arrays.asList(Integer.class.getName(), Book.class.getName()));
-//        cfg.setBinaryConfiguration(bCfg);
-
         this.ignite = Ignition.start(cfg);
+        //testStream(ignite);
 
-        CacheConfiguration<Integer, Book> cacheCfg = new CacheConfiguration<>("test");
-        this.cache = ignite.getOrCreateCache(cacheCfg);//.withKeepBinary();
-        testSimple(ignite);
-
-    }
-
-    public static void testSimple(Ignite ignite){
-        IgniteCache<Integer, Object> bookC = ignite.getOrCreateCache("bookC");
-        Book b1 = new Book("1", "auth1", "title1");
-        Book b2 = new Book("1", "auth2", "title1");
-        Book b3 = new Book("1", "auth3", "title1");
-        Book b4 = new Book("1", "auth4", "title1");
-        Book b5 = new Book("1", "auth5", "title1");
-
-        bookC.put(1, b1);
-        bookC.put(2, b2);
-        bookC.put(3, b3);
-        bookC.put(4, b4);
-        bookC.put(5, b5);
-
-        System.out.println("bookC size: " + bookC.size(CachePeekMode.ALL));
-        checkNodes(ignite);
-
-    }
-
-    public static void checkNodes(Ignite ignite){
-        Collection<Long> lists = ignite.compute().broadcast(new IgniteCallable<Long>() {
-
-            @IgniteInstanceResource
-            private Ignite ign;
-
-            @Override
-            public Long call() throws Exception {
-                System.out.println("server name: " + ignite.cluster().localNode().id() + " cache size: " + ign.getOrCreateCache("bookC").localSize(CachePeekMode.ALL));
-
-                return 1l;
-            }
-        });
-        lists.forEach(i-> System.out.println(i));
-    }
-
-    public static void testAff(Ignite ignite){
-        //AffinityKey bookKey1 = new AffinityKey("myBookId1", "myLibId");
-        //AffinityKey bookKey2 = new AffinityKey("myBookId2", "myLibId");
-
-        //IgniteCache<Object, Library> libC = ignite.getOrCreateCache("libC");
-        //libC.put("myLibId", new Library("myLibId", "myLibrary"));
-
-        //bookC.put("myLibId", new Library("myLibId", "myLibrary"));
-        //bookC.put(bookKey1, new Book(bookKey1, "auth1", "title1"));
-        //bookC.put(bookKey2, new Book(bookKey2, "auth2", "title2"));
     }
 
     @Bean
--- java/igniteClient/src/main/java/com/ignite/config/ShellCommands.java
+++ java/igniteClient/src/main/java/com/ignite/config/ShellCommands.java
@@ -3,29 +3,26 @@ package com.ignite.config;
 import com.ignite.domain.Book;
 import com.ignite.domain.Library;
 import com.ignite.util.Utils;
-import org.apache.ignite.DataRegionMetrics;
-import org.apache.ignite.Ignite;
-import org.apache.ignite.IgniteCache;
-import org.apache.ignite.IgniteCompute;
+import org.apache.ignite.*;
 import org.apache.ignite.binary.BinaryObject;
 import org.apache.ignite.cache.CachePeekMode;
 import org.apache.ignite.cache.query.QueryCursor;
 import org.apache.ignite.cache.query.ScanQuery;
 import org.apache.ignite.internal.util.typedef.F;
+import org.apache.ignite.lang.IgniteBiInClosure;
 import org.apache.ignite.lang.IgniteBiPredicate;
 import org.apache.ignite.lang.IgniteCallable;
 import org.apache.ignite.lang.IgniteClosure;
 import org.apache.ignite.resources.IgniteInstanceResource;
+import org.apache.ignite.stream.StreamReceiver;
+import org.apache.ignite.stream.StreamVisitor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
 import javax.cache.Cache;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.stream.IntStream;
 
 import static com.ignite.util.Utils.createDataSize;
@@ -42,6 +39,93 @@ public class ShellCommands {
         this.ignite = ignite;
         this.cache = cache;
     }
+
+    @ShellMethod("computeQuery")
+    public void test_stream(){
+        IgniteCache<Integer, String> bookC = ignite.getOrCreateCache("bookC");
+        IgniteCache<Integer, String> bookD = ignite.getOrCreateCache("bookD");
+
+        bookD.put(1,"v1");
+        bookD.put(2,"v2");
+        bookD.put(3,"v3");
+
+        bookC.removeAll();
+        System.out.println("bookC cleaned size: " + bookC.size(CachePeekMode.ALL));
+        checkNodes(ignite);
+
+        try(IgniteDataStreamer<Integer, String> stream = ignite.dataStreamer("bookC")){
+            stream.allowOverwrite(true);
+            stream.receiver(new StreamVisitor<Integer, String>(){
+                @IgniteInstanceResource
+                private Ignite ign;
+
+                @Override
+                public void apply(IgniteCache<Integer, String> cache, Map.Entry<Integer, String> e) {
+                    System.out.println("XYIXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+                    System.out.println("input_key: " + e.getKey());
+                    System.out.println("input_value: " + e.getValue());
+                    //cache.put(e.getKey(), e.getValue());
+                    IgniteCache<Integer, String> c = ign.getOrCreateCache("bookD");
+                    String s = c.get(e.getKey());
+                    System.out.println("value from another cache: " + s);
+                }
+            });
+            stream.addData(2, "test");
+        }
+
+        System.out.println("bookC inited size: " + bookC.size(CachePeekMode.ALL));
+        checkNodes(ignite);
+
+    }
+
+    public static void testSimple(Ignite ignite){
+        IgniteCache<Integer, Object> bookC = ignite.getOrCreateCache("bookC");
+        Book b1 = new Book("1", "auth1", "title1");
+        Book b2 = new Book("1", "auth2", "title1");
+        Book b3 = new Book("1", "auth3", "title1");
+        Book b4 = new Book("1", "auth4", "title1");
+        Book b5 = new Book("1", "auth5", "title1");
+
+        bookC.put(1, b1);
+        bookC.put(2, b2);
+        bookC.put(3, b3);
+        bookC.put(4, b4);
+        bookC.put(5, b5);
+
+        System.out.println("bookC size: " + bookC.size(CachePeekMode.ALL));
+        checkNodes(ignite);
+
+    }
+
+    public static void checkNodes(Ignite ignite){
+        Collection<Long> lists = ignite.compute().broadcast(new IgniteCallable<Long>() {
+
+            @IgniteInstanceResource
+            private Ignite ign;
+
+            @Override
+            public Long call() throws Exception {
+                System.out.println("server name: " + ignite.cluster().localNode().id() + " cache size: " + ign.getOrCreateCache("bookC").localSize(CachePeekMode.ALL));
+
+                return 1l;
+            }
+        });
+        lists.forEach(i-> System.out.println(i));
+    }
+
+    public static void testAff(Ignite ignite){
+        //AffinityKey bookKey1 = new AffinityKey("myBookId1", "myLibId");
+        //AffinityKey bookKey2 = new AffinityKey("myBookId2", "myLibId");
+
+        //IgniteCache<Object, Library> libC = ignite.getOrCreateCache("libC");
+        //libC.put("myLibId", new Library("myLibId", "myLibrary"));
+
+        //bookC.put("myLibId", new Library("myLibId", "myLibrary"));
+        //bookC.put(bookKey1, new Book(bookKey1, "auth1", "title1"));
+        //bookC.put(bookKey2, new Book(bookKey2, "auth2", "title2"));
+    }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     @ShellMethod("computeQuery")
     public void exec(){
