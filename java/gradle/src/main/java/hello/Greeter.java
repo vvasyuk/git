d5b49745074f452c3260c4e1c4b2e31432d80441
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2018-04-15 21:58:17 +0300

    upd

--- java/gradle/src/main/java/hello/Greeter.java
+++ java/gradle/src/main/java/hello/Greeter.java
@@ -5,25 +5,39 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 import java.io.File;
-//import com.typesafe.config.Config;
-//import com.typesafe.config.ConfigFactory;
+import java.util.List;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 
 @Configuration
 public class Greeter {
-//  public Greeter(@Value("${config}") String configFileName, ConfigurableEnvironment environment) throws Exception {
-//    final File configFile = new File(configFileName);
-//    Config config = ConfigFactory.parseFile(configFile);
-//    System.out.println(config.getString("VAR"));
-//
-//    environment.getPropertySources().addLast(new org.springframework.core.env.PropertySource<Config>("configFile", config) {
-//      @Override
-//      public String getProperty(String s) {
-//        try{
-//          return source.getString(s);
-//        }catch(Exception e){
-//          return null;
-//        }
-//      }
-//    });
-//  }
+  public Greeter(@Value("${config}") String configFileName, ConfigurableEnvironment environment) throws Exception {
+    final File configFile = new File(configFileName);
+    Config config = ConfigFactory.parseFile(configFile);
+    System.out.println(config.getString("VAR"));
+
+    environment.getPropertySources().addLast(new org.springframework.core.env.PropertySource<Config>("configFile", config) {
+      @Override
+      public String getProperty(String s) {
+        try{
+          return source.getString(s);
+        }catch(Exception e){
+          return null;
+        }
+      }
+    });
+
+
+      Config c = (Config) environment.getPropertySources().get("configFile").getSource();
+
+      List<Integer> listInt = c.getIntList("VARAR");
+      System.out.println(listInt.get(0));
+      System.out.println(environment.getProperty("VAR", String.class));
+
+      //System.out.println(c.getInt("VARL"));
+
+      System.out.println(c.getLong("VARL"));
+
+  }
 }
