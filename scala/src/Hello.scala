2604a808967388a615fa79141dba4368a942f2ef
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2018-10-28 11:55:30 +0200

    upd

--- scala/src/Hello.scala
+++ scala/src/Hello.scala
@@ -1,36 +1,55 @@
+import scala.collection.mutable
+
 object Hello {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
 
-    val simpleClass = new SimpleClass(1, null)
-
-
-    try{
-      val caches = Caches.getCaches(simpleClass)
-      import caches._
-      println("tCache="+tCache)
-    } catch {
-      case e: Exception => println("exception caught: " + e.getMessage);
+    val thread1 = new Thread {
+      override def run {
+        Thread.sleep(7000)
+        Keeper.map(2) = "inited"
+      }
     }
 
-    println("End")
+    val thread2 = new Thread {
+      override def run {
+        for (i <- 1 to 10) {
+          Thread.sleep(2000)
+          try {
+            val caches = Caches.getCaches(Keeper.map)
+            import caches._
+            println("tCache=" + tCache)
+          } catch {
+            case e: Exception => println("exception caught: " + e.getMessage);
+          }
+        }
+      }
+    }
+
+    thread1.start
+    thread2.start
   }
 }
 
 trait Caches {
-  val tCache: String
+  def tCache: String
 }
 
-object Caches {
-  def getCaches(simpleClass: SimpleClass) = new Caches {
-    val tCache: String = initCache(simpleClass)
+object Caches extends Caches{
+  lazy val tCache: String = initCache(Keeper.map)
+
+  def getCaches(m: mutable.Map[Int, String]) = new Caches {
+    lazy val tCache: String = initCache(m)
   }
 
-  def initCache(simpleClass: SimpleClass): String = {
-
-    if(simpleClass.myString==null){
+  def initCache(m: mutable.Map[Int, String]): String = {
+    if(m(2)==null){
       throw new Exception("myString is null");
     }
-    simpleClass.myString
+    m(2)
   }
+}
+
+object Keeper{
+  var map = scala.collection.mutable.Map(1 -> "one")
+  map += (2 -> null)
 }
