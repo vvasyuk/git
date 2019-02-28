73b712a2c3761fc5136b1d6e261628a12457a005
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2019-02-28 22:03:10 +0200

    upd

--- bash/hello.sh
+++ bash/hello.sh
@@ -1,8 +1,39 @@
-#!/bin/bash
+####!/bin/bash
+#### https://devhints.io/bash#functions
 
+name="John"
+echo ${name}
+
+###    IF    ###
+ENV1=dev2
+if [[ "dev1" == ${ENV1} ]] || [[ "dev2" == ${ENV1} ]]; then
+  echo "its dev"
+else
+  echo "its not dev"
+fi
+
+if [[ -z "$string" ]]; then
+  echo "String is empty"
+elif [[ -n "$string" ]]; then
+  echo "String is not empty"
+fi
+###    IF    ###
+
+###    FUNC    ####
 func(){
-    echo func
+    echo func $1
 }
+func par
+###    FUNC    ####
 
-echo Hello World
-func
+echo "I'm in $(pwd)"
+
+###    LOOPS    ###
+for i in $(pwd)/.*; do
+  echo $i
+done
+
+for i in {1..5}; do
+    echo "Welcome $i"
+done
+###    LOOPS    ###
