d1dc7cc0d06e489553fc464740c87d9e86315e39
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2019-06-24 18:08:58 +0300

    upd

--- java/general/src/main/java/com/tryout/JavaApplication.java
+++ java/general/src/main/java/com/tryout/JavaApplication.java
@@ -90,14 +90,7 @@ public class JavaApplication {
 		//System.out.println(GreatestCommonDivisor.execute(30, 20));
 		//LeastCommonMultuply
 		//System.out.println(LeastCommonMultuply.execute(15, 20));
-		System.out.println(1%3);
-		System.out.println(2%3);
-		System.out.println(3%3);
-		System.out.println(4%3);
-		System.out.println(5%3);
-		System.out.println(6%3);
-		System.out.println(7%3);
-		System.out.println(8%3);
-		System.out.println(9%3);
+		//MoneyChange
+		System.out.println(MoneyChange.execute(28));
 	}
 }
---
+++ java/general/src/main/java/com/tryout/generalPuzzles/MoneyChange.java
@@ -0,0 +1,47 @@
+package com.tryout.generalPuzzles;
+
+public class MoneyChange {
+
+    public static int execute(int c) {
+        //Compute the minimum number of coins needed
+        //to change the given value into coins with denominations
+        //1, 5, and 10.
+
+        int[] nominals=new int[3];
+        nominals[0]=1;
+        nominals[1]=5;
+        nominals[2]=10;
+        int res=0;
+
+//        for(int i = nominals.length-1; i>=0; i--){
+//            int tmp=c/nominals[i];
+//            res+=tmp;
+//            c-=nominals[i]*tmp;
+//        }
+//        return res;
+        return recHelper(c, nominals, nominals.length-1, 0);
+    }
+
+//    private static int recHelper(int change, int[] nominals, int idx){
+//
+//        int count=change/nominals[idx];
+//        change-=count*nominals[idx];
+//
+//        if(idx>0){
+//            count+=recHelper(change, nominals, --idx);
+//        }
+//        return count;
+//    }
+
+    private static int recHelper(int change, int[] nominals, int idx, int sum){
+
+        int count=change/nominals[idx];
+        change-=count*nominals[idx];
+        sum+=count;
+
+        if(idx>0){
+            sum=recHelper(change, nominals, --idx, sum);
+        }
+        return sum;
+    }
+}
