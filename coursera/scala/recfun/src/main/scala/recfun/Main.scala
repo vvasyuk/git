3a8cfca62580bea7c22896a351d640765b5e4b1f
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2019-08-03 21:33:39 +0300

    upd

--- coursera/scala/recfun/src/main/scala/recfun/Main.scala
+++ coursera/scala/recfun/src/main/scala/recfun/Main.scala
@@ -72,9 +72,16 @@ object Main {
     }
     recHelper(chars)
   }
-    //  /**
-    //   * Exercise 3
-    //   */
-    //    def countChange(money: Int, coins: List[Int]): Int = ???
-
+      /**
+       * Exercise 3
+       */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money < 0 || coins.isEmpty){
+      0
+    } else if (money == 0) {
+      1
+    } else {
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    }
+  }
 }
