8ac6337a6ff639f139728e280496bf79657fedae
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2019-08-03 20:24:28 +0300

    upd

--- coursera/scala/recfun/src/main/scala/recfun/Main.scala
+++ coursera/scala/recfun/src/main/scala/recfun/Main.scala
@@ -3,25 +3,78 @@ package recfun
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
-    for (row <- 0 to 10) {
-      for (col <- 0 to row)
-        print(pascal(col, row) + " ")
-      println()
+    //var in = "(if (zero? x) max (/ 1 x))".toList
+    //var in = "I told him (that it’s not (yet) done). (But he wasn’t listening)".toList
+    //var in = ":-)".toList
+    //var in = "())(".toList
+    //println(balance(in))
+    //    for (row <- 0 to 10) {
+    //      for (col <- 0 to row)
+    //        print(col + row + " ")
+    //      //print(pascal(col, row) + " ")
+    //      println()
+    //    }
+  }
+
+  /**
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || r == 0 || c == r) {
+      1
+    } else {
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
     }
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = ???
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    var openBracketCnt = 0;
+
+    def recHelper(chars: List[Char]): Boolean = {
+      chars match {
+        case _::Nil => {
+          val h = chars.head
+          if (h.equals('(')) {
+            return false
+          } else if (h.equals(')')){
+            openBracketCnt -= 1
+            if (openBracketCnt == 0) {
+              return true
+            } else {
+              return false
+            }
+          } else {
+            return true
+          }
+        }
+        case _=> {
+          val h = chars.head
+          val t = chars.tail
+
+          if (h.equals('(')) {
+            openBracketCnt += 1
+            recHelper(t)
+          } else if (h.equals(')')) {
+            if (openBracketCnt > 0) {
+              openBracketCnt -= 1
+            } else {
+              return false
+            }
+            recHelper(t)
+          } else {
+            recHelper(t)
+          }
+        }
+      }
+    }
+    recHelper(chars)
   }
+    //  /**
+    //   * Exercise 3
+    //   */
+    //    def countChange(money: Int, coins: List[Int]): Int = ???
+
+}
