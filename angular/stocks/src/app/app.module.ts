6716de7ab611493f9e4220338dae48392a0fa448
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2019-08-05 22:57:33 +0300

    upd

--- angular/stocks/src/app/app.module.ts
+++ angular/stocks/src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { StocksService } from './services/stocks.service';
+import { AppRoutes } from './app.routes';
 
 import { AppComponent } from './app.component';
 import { SummaryComponent } from './components/summary/summary.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SummaryComponent,
-    DashboardComponent
+    DashboardComponent,
+    ManageComponent,
+    FormsModule,
+    
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    AppRoutes,
   ],
   providers: [StocksService],
   bootstrap: [AppComponent]
