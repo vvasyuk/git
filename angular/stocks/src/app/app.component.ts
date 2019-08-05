9ed199daf6b4388aea5ff630aeca7f02d1e4ff32
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2019-08-05 22:41:56 +0300

    upd

---
+++ angular/stocks/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { StocksService, StockInterface } from './services/stocks.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  // stocks: Array<StockInterface>;
+
+  // constructor(service: StocksService) {
+  //   service.load(['AAPL']).subscribe(stocks => {
+  //     this.stocks = stocks;
+  //   });}
+}
