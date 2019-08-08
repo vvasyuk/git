25e7bf8e87c0eb61d74c1308970b3ef817539755
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2019-08-08 22:49:58 +0300

    upd

--- java/general/src/main/java/com/tryout/algorithms1/w2/Deque.java
+++ java/general/src/main/java/com/tryout/algorithms1/w2/Deque.java
@@ -1,34 +1,120 @@
 package com.tryout.algorithms1.w2;
 
+import java.util.Iterator;
+
 public class Deque<Item> implements Iterable<Item> {
+//    Corner cases.  Throw the specified exception for the following corner cases:
+//
+//    Throw a java.lang.IllegalArgumentException if the client calls either addFirst() or addLast() with a null argument.
+//    Throw a java.util.NoSuchElementException if the client calls either removeFirst() or removeLast when the deque is empty.
+//    Throw a java.util.NoSuchElementException if the client calls the next() method in the iterator when there are no more items to return.
+//    Throw a java.lang.UnsupportedOperationException if the client calls the remove() method in the iterator.
+
+    Node first;
+    Node last;
+    int size;
+
     // construct an empty deque
-    public Deque() {}
+    public Deque() {
+        first = null;
+        last = null;
+    }
 
     // is the deque empty?
-    public boolean isEmpty() {}
+    public boolean isEmpty() {
+        return first == null;
+    }
 
     // return the number of items on the deque
-    public int size() {}
+    public int size() {
+        return size;
+    }
 
     // add the item to the front
-    public void addFirst(Item item) {}
+    public void addFirst(Item item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+        oldFirst.prev = first;
+        size++;
+    }
 
     // add the item to the back
-    public void addLast(Item item) {}
+    public void addLast(Item item) {
+        Node newLast = new Node();
+        newLast.item = item;
+        newLast.prev = last;
+        last.next = newLast;
+        last = newLast;
+        size++;
+    }
 
     // remove and return the item from the front
-    public Item removeFirst() {}
+    public Item removeFirst() {
+        Item tmp = first.item;
+        first = first.next;
+        size--;
+        return tmp;
+    }
 
     // remove and return the item from the back
-    public Item removeLast() {}
+    public Item removeLast() {
+        Item tmp = last.item;
+        last = last.prev;
+        size--;
+        return tmp;
+    }
 
     // return an iterator over items in order from front to back
-    public Iterator<Item> iterator() {}
+    public Iterator<Item> iterator() { return new MyIterator(); }
 
     // unit testing (required)
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Deque<String> deq = new Deque<>();
+        System.out.println(deq.size);
+        deq.addFirst("one");
+        deq.addFirst("two");
+        System.out.println(deq.removeLast());
+        System.out.println(deq.removeLast());
+    }
+
+    private class MyIterator implements Iterator<Item> {
+        private Node current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Item next() {
+            Item tmp = current.item;
+            current = current.next;
+            return tmp;
+        }
+    }
+
+    private class Node{
+        Item item;
+        Node next;
+        Node prev;
+    }
 }
 
+//    public Iterator<Item> iterator() { return new ListIterator(); }
+//private class ListIterator implements Iterator<Item>
+//{
+//    private Node current = first;
+//    public boolean hasNext() { return current != null; }
+//    public void remove() { /* not supported */ }
+//    public Item next()
+//    {
+//        Item item = current.item;
+//        current = current.next;
+//        return item;
+//    }
+//}
 
 //public class LinkedStackOfStrings
 //{
--- java/general/src/main/java/com/tryout/algorithms1/w2/RandomizedQueue.java
+++ java/general/src/main/java/com/tryout/algorithms1/w2/RandomizedQueue.java
@@ -1,28 +1,28 @@
 package com.tryout.algorithms1.w2;
 
-public class RandomizedQueue<Item> implements Iterable<Item> {
-    //Implementation. Stack (without pop) or queue (without dequeue).
-    // construct an empty randomized queue
-    public RandomizedQueue() {}
-
-    // is the randomized queue empty?
-    public boolean isEmpty() {}
-
-    // return the number of items on the randomized queue
-    public int size() {}
-
-    // add the item
-    public void enqueue(Item item) {}
-
-    // remove and return a random item
-    public Item dequeue() {}
-
-    // return a random item (but do not remove it)
-    public Item sample() {}
-
-    // return an independent iterator over items in random order
-    public Iterator<Item> iterator() {}
-
-    // unit testing (required)
-    public static void main(String[] args) {}
-}
+//public class RandomizedQueue<Item> implements Iterable<Item> {
+//    //Implementation. Stack (without pop) or queue (without dequeue).
+//    // construct an empty randomized queue
+//    public RandomizedQueue() {}
+//
+//    // is the randomized queue empty?
+//    public boolean isEmpty() {}
+//
+//    // return the number of items on the randomized queue
+//    public int size() {}
+//
+//    // add the item
+//    public void enqueue(Item item) {}
+//
+//    // remove and return a random item
+//    public Item dequeue() {}
+//
+//    // return a random item (but do not remove it)
+//    public Item sample() {}
+//
+//    // return an independent iterator over items in random order
+//    public Iterator<Item> iterator() {}
+//
+//    // unit testing (required)
+//    public static void main(String[] args) {}
+//}
