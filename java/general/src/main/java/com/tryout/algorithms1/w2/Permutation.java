78d4edc4fe15ae8122d3d914fc22d3ec090ff3b8
Vadym <vvasyuk@gmail.com>
2019-08-10 13:44:07 +0300

    cfhg

--- java/general/src/main/java/com/tryout/algorithms1/w2/Permutation.java
+++ java/general/src/main/java/com/tryout/algorithms1/w2/Permutation.java
@@ -1,4 +1,17 @@
 package com.tryout.algorithms1.w2;
 
+import edu.princeton.cs.algs4.StdIn;
+
 public class Permutation {
+    public static void main(String[] args) {
+        RandomizedQueue<String> rq = new RandomizedQueue();
+        int k = Integer.parseInt(args[0]);
+        while(!StdIn.isEmpty()) {
+            rq.enqueue(StdIn.readString());
+        }
+        while(k>0) {
+            System.out.println(rq.dequeue());
+            k--;
+        }
+    }
 }
--- java/general/src/main/java/com/tryout/algorithms1/w2/RandomizedQueue.java
+++ java/general/src/main/java/com/tryout/algorithms1/w2/RandomizedQueue.java
@@ -2,6 +2,8 @@ package com.tryout.algorithms1.w2;
 
 import edu.princeton.cs.algs4.StdRandom;
 
+import java.util.Iterator;
+
 //public class RandomizedQueue<Item> implements Iterable<Item> {
 public class RandomizedQueue<Item> {
     //Implementation. Stack (without pop) or queue (without dequeue).
@@ -26,17 +28,29 @@ public class RandomizedQueue<Item> {
 
     // add the item
     public void enqueue(Item item) {
+        if (size == a.length) resize(2 * a.length);
         a[size++] = item;
     }
 
     // remove and return a random item
     public Item dequeue() {
+        if (size == 0) {
+            throw new java.util.NoSuchElementException();
+        }
         size--;
         int r = StdRandom.uniform(0, size);
         Item tmp = a[r];
         a[r] = a[size];
         a[size] = null;
+        if (size > 0 && size == a.length/4) resize(a.length/2);
         return tmp;
+    }
+
+    private void resize(int capacity) {
+        Item[] copy = (Item[])new Object[capacity];
+        for (int i = 0; i < size; i++)
+            copy[i] = a[i];
+        a = copy;
     }
 
     // return a random item (but do not remove it)
@@ -46,7 +60,18 @@ public class RandomizedQueue<Item> {
     }
 
     // return an independent iterator over items in random order
-//    public Iterator<Item> iterator() {}
+    public Iterator<Item> iterator() {
+        return new MyArrayIterator();
+    }
+
+    private class MyArrayIterator implements Iterator<Item> {
+        private int i = size;
+        public boolean hasNext() { return i > 0; }
+        public void remove() {
+            throw new java.lang.UnsupportedOperationException();
+        }
+        public Item next() { return a[--i]; }
+    }
 
     // unit testing (required)
     public static void main(String[] args) {
