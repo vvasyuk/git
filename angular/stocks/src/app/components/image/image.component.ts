2164b4c2d607a4e24cff3531f14a4cfd5d863880
Vadym <vvasyuk@gmail.com>
2019-08-13 00:16:20 +0300

    upd

--- angular/stocks/src/app/components/image/image.component.ts
+++ angular/stocks/src/app/components/image/image.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ImageService } from '../../services/image.service';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+
+// npm install --save @types/node
+// npm install pdfmake --save
+// npm install pdfmake-unicode --save
+// npm install @types/pdfmake --save-dev
+declare var require: any;
+const pdfMakeX = require('pdfmake/build/pdfmake.js');
+const pdfFontsX = require('pdfmake-unicode/dist/pdfmake-unicode.js');
+pdfMakeX.vfs = pdfFontsX.pdfMake.vfs;
 
 @Component({
   selector: 'app-image',
@@ -7,6 +17,7 @@ import { ImageService } from '../../services/image.service';
   styleUrls: ['./image.component.scss']
 })
 export class ImageComponent {
+
 
   imgUrl: string = 'http://localhost:8080/img';
   //imgUrl: string = 'https://picsum.photos/200/300/?random';
@@ -47,6 +58,24 @@ export class ImageComponent {
           [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
           [ 'Bold value', 'Val 2', 'Val 3', 'Val 4' ]
         ];
+  }
+
+  getPdf(){
+    const docDefinition = {
+      content: [{
+          table: {
+              headerRows: 1,
+              widths: ['*', 'auto', 100, '*'],
+              // body: [
+              //     ['First', 'Second', 'Third', 'Последняя'],
+              //     ['Value 1', 'Value 2', 'Value 3', 'Value 4'],
+              //     [{ text: 'Bold value', bold: true }, 'Val 2', 'Val 3', 'Чё']
+              // ]
+              body: this.csvToShow
+          }
+      }]
+  };
+  pdfMake.createPdf(docDefinition).download('pdfmake.pdf');
   }
 
 }
