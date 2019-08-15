9d2b1a6a41055f0e4c9a934c45b1adac71ed3e53
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2019-08-15 22:18:58 +0300

    upd

--- coursera/algorithms1/w3/Main.java
+++ coursera/algorithms1/w3/Main.java
@@ -1,42 +1,42 @@
-public class Main {
-    public static void main(String[] args) {
-
-        // read the n points from a file
-        In in = new In(args[0]);
-        int n = in.readInt();
-        Point[] points = new Point[n];
-        for (int i = 0; i < n; i++) {
-            int x = in.readInt();
-            int y = in.readInt();
-            points[i] = new Point(x, y);
-        }
-
-        // draw the points
-        StdDraw.enableDoubleBuffering();
-        StdDraw.setXscale(0, 32768);
-        StdDraw.setYscale(0, 32768);
-        for (Point p : points) {
-            p.draw();
-        }
-        StdDraw.show();
-
-        // print and draw the line segments
-        FastCollinearPoints collinear = new FastCollinearPoints(points);
-        for (LineSegment segment : collinear.segments()) {
-            StdOut.println(segment);
-            segment.draw();
-        }
-        StdDraw.show();
-    }
-}
-
-//% cat input6.txt        % cat input8.txt
-//        6                       8
-//        19000  10000             10000      0
-//        18000  10000                 0  10000
-//        32000  10000              3000   7000
-//        21000  10000              7000   3000
-//        1234   5678             20000  21000
-//        14000  10000              3000   4000
-//        14000  15000
-//        6000   7000
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        // read the n points from a file
+//        In in = new In(args[0]);
+//        int n = in.readInt();
+//        Point[] points = new Point[n];
+//        for (int i = 0; i < n; i++) {
+//            int x = in.readInt();
+//            int y = in.readInt();
+//            points[i] = new Point(x, y);
+//        }
+//
+//        // draw the points
+//        StdDraw.enableDoubleBuffering();
+//        StdDraw.setXscale(0, 32768);
+//        StdDraw.setYscale(0, 32768);
+//        for (Point p : points) {
+//            p.draw();
+//        }
+//        StdDraw.show();
+//
+//        // print and draw the line segments
+//        FastCollinearPoints collinear = new FastCollinearPoints(points);
+//        for (LineSegment segment : collinear.segments()) {
+//            StdOut.println(segment);
+//            segment.draw();
+//        }
+//        StdDraw.show();
+//    }
+//}
+//
+////% cat input6.txt        % cat input8.txt
+////        6                       8
+////        19000  10000             10000      0
+////        18000  10000                 0  10000
+////        32000  10000              3000   7000
+////        21000  10000              7000   3000
+////        1234   5678             20000  21000
+////        14000  10000              3000   4000
+////        14000  15000
+////        6000   7000
--- coursera/algorithms1/w3/Point.java
+++ coursera/algorithms1/w3/Point.java
@@ -1,13 +1,94 @@
-import java.util.Comparator;
+import edu.princeton.cs.algs4.StdDraw;
 
 public class Point implements Comparable<Point> {
-    public Point(int x, int y)                         // constructs the point (x, y)
 
-    public   void draw()                               // draws this point
-    public   void drawTo(Point that)                   // draws the line segment from this point to that point
-    public String toString()                           // string representation
+    private final int x;     // x-coordinate of this point
+    private final int y;     // y-coordinate of this point
 
-    public               int compareTo(Point that)     // compare two points by y-coordinates, breaking ties by x-coordinates
-    public            double slopeTo(Point that)       // the slope between this point and that point
-    public Comparator<Point> slopeOrder()              // compare two points by slopes they make with this point
+    public Point(int x, int y) {
+        /* DO NOT MODIFY */
+        this.x = x;
+        this.y = y;
+    }
+
+    public void draw() {
+        /* DO NOT MODIFY */
+        StdDraw.point(x, y);
+    }
+
+    public void drawTo(Point that) {
+        /* DO NOT MODIFY */
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    /**
+     * Returns the slope between this point and the specified point.
+     * Formally, if the two points are (x0, y0) and (x1, y1), then the slope
+     * is (y1 - y0) / (x1 - x0). For completeness, the slope is defined to be
+     * +0.0 if the line segment connecting the two points is horizontal;
+     * Double.POSITIVE_INFINITY if the line segment is vertical;
+     * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
+     *
+     * @param  that the other point
+     * @return the slope between this point and the specified point
+     */
+    public double slopeTo(Point that) {
+        if ( y == that.y ) return 0;
+        if ( x == that.x ) return Double.POSITIVE_INFINITY;
+        if ( x == that.x && )
+    }
+
+    /**
+     * Compares two points by y-coordinate, breaking ties by x-coordinate.
+     * Formally, the invoking point (x0, y0) is less than the argument point
+     * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
+     *
+     * @param  that the other point
+     * @return the value <tt>0</tt> if this point is equal to the argument
+     *         point (x0 = x1 and y0 = y1);
+     *         a negative integer if this point is less than the argument
+     *         point; and a positive integer if this point is greater than the
+     *         argument point
+     */
+    public int compareTo(Point that) {
+        if ( y == that.y && x == that.x) return 0;
+        if ( y < that.y || ( y == that.y && x < that.x)) return -1;
+        return 1;
+    }
+
+    /**
+     * Compares two points by the slope they make with this point.
+     * The slope is defined as in the slopeTo() method.
+     *
+     * @return the Comparator that defines this ordering on points
+     */
+//    public Comparator<Point> slopeOrder() {
+//        /* YOUR CODE HERE */
+//    }
+
+
+    /**
+     * Returns a string representation of this point.
+     * This method is provide for debugging;
+     * your program should not rely on the format of the string representation.
+     *
+     * @return a string representation of this point
+     */
+    public String toString() {
+        /* DO NOT MODIFY */
+        return "(" + x + ", " + y + ")";
+    }
+
+    /**
+     * Unit tests the Point data type.
+     */
+    public static void main(String[] args) {
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(2,2);
+        System.out.println(p1.compareTo(p2));
+//        p1.draw();
+//        p2.draw();
+//        p1.drawTo(p2);
+
+    }
 }
