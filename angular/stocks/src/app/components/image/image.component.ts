824d1757ce73dd4f3499d353cd46f2ec870fae73
Vadym <vvasyuk@gmail.com>
2019-08-26 02:13:29 +0300

    upd

--- angular/stocks/src/app/components/image/image.component.ts
+++ angular/stocks/src/app/components/image/image.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ImageService } from '../../services/image.service';
 import * as pdfMake from 'pdfmake/build/pdfmake';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 // npm install --save @types/node
 // npm install pdfmake --save
@@ -27,7 +29,8 @@ export class ImageComponent {
   text: string;
 
 
-  constructor(private imageService: ImageService) {}
+  constructor(private imageService: ImageService,
+    private http: HttpClient) {}
 
   createImageFromBlob(image: Blob) {
     let reader = new FileReader();
@@ -66,6 +69,23 @@ export class ImageComponent {
     this.csvToShow = [
           [ 'First', 'Second', 'Third', 'The last one' ],
           [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
           [ 'Bold value', 'Val 2', 'Val 3', 'Val 4' ]
         ];
   }
@@ -85,7 +105,59 @@ export class ImageComponent {
           }
       }]
   };
+  console.info('1')
+  console.info('1')
+  console.info('1')
+  console.info('1')
+  console.info('1')
+  console.info('1')
   pdfMake.createPdf(docDefinition).download('pdfmake.pdf');
+  console.info('2')
+  }
+
+  seq: string = ""
+  getSeq() {
+    this.http.get('http://www.mocky.io/v2/5d62fb55330000d03e55a71b', {responseType: 'text'})
+    .subscribe(a => {
+      this.http.get('http://www.mocky.io/v2/5d6302a1330000403c55a729', {responseType: 'text'}).subscribe(b => {
+        this.seq = a+b
+      });
+    });
+  }
+
+  seq2: string = ""
+  getSeq2() {
+    var observable = Observable.create(function(observer) {
+      console.info('0,1')
+      const docDefinition = {
+        content: [{
+            table: {
+                headerRows: 1,
+                widths: ['*', 'auto', 100, '*'],
+                body: [
+                    ['First', 'Second', 'Third', 'Последняя'],
+                    ['Value 1', 'Value 2', 'Value 3', 'Value 4'],
+                    [{ text: 'Bold value', bold: true }, 'Val 2', 'Val 3', 'Чё']
+                ]
+            }
+        }]
+    };
+    pdfMake.createPdf(docDefinition).download('pdfmake.pdf');
+      console.info('0,2')
+      observer.next(0);
+      observer.complete();});
+
+    console.info('1')
+    observable.subscribe(() => this.seq2='xyi')
+
+    // let obs = new Observable<void>(observer => observer.complete());
+    // obs.subscribe(() => this.seq2='xyi')
+    // this.http.get('http://www.mocky.io/v2/5d62fb55330000d03e55a71b', {responseType: 'text'})
+    // .subscribe(a => {
+    //   obs.subscribe(() => {
+    //     this.seq2 = a+'xyi'
+    //   });
+    // });
   }
 
 }
