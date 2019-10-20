cd90e44e20b293590de64346e418efadd08614bb
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2019-10-20 22:29:54 +0300

    upd

--- perl/boxWrap.pl
+++ perl/boxWrap.pl
@@ -1,5 +1,11 @@
-#my $s = shift(@ARGV);
-#print "$s"
-
-@lines = <>;
-print @lines;
+print $1;
+open AAAA,$1;
+my @array = <AAAA>; # read the file into an array of lines
+close AAAA;
+my $max = -1;
+for my $line (@array) {
+    if (length($line) > $max) {
+        $max = length($line);
+    }
+}
+print $max
