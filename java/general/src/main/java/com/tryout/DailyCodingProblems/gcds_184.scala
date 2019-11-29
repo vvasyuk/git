d428e26cb6439e6f4fb463620a1b32b02580f6d2
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2019-11-29 20:50:24 +0200

    upd

---
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/gcds_184.scala
@@ -0,0 +1,31 @@
+package com.tryout.DailyCodingProblems
+
+object gcds_184 {
+
+  def execute(seq: Seq[Int]): Unit = {
+    //val a = 1
+    //val b = 9
+    //println(gcd(a,b))
+    println(gcdHelper(seq))
+  }
+
+  def gcdHelper(seq: Seq[Int]):Int={
+    val res = seq.foldLeft(0)((a,e) => gcd(e,a))
+    res
+  }
+
+  def gcd(x: Int, y: Int):Int = {
+    if(y==0){
+      x
+    }else if (x%y==0){
+      y
+    }else{
+      gcd(y,x%y)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    println("gcds_184 started")
+    execute(Seq(42, 56, 14))
+  }
+}
