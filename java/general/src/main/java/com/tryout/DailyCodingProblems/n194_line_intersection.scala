fbbc7d00d525c805c2bee5ad0e4aa17ee8af6236
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2019-12-10 22:28:36 +0200

    upd

---
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/n194_line_intersection.scala
@@ -0,0 +1,20 @@
+package com.tryout.DailyCodingProblems
+
+object n194_line_intersection {
+
+  def execute(l1: Array[Int], l2: Array[Int]): Unit = {
+
+    for(n<-l1.zipWithIndex;
+        m<-l2.zipWithIndex if m._1 > n._1
+        ){
+      println(n._2 + " : " + m._2)
+    }
+
+  }
+
+  def main(args: Array[String]):Unit={
+    val l1 = Array(1,3,5)
+    val l2 = Array(2,4,6)
+    execute(l1, l2)
+  }
+}
--- python/python3/learn/test.py
+++ python/python3/learn/test.py
@@ -1,4 +1,14 @@
-s = "hello world"
-print("string: " + s)
-print("reverse: " + s[::-1])
-print("length: " + str(len(s)))
+# s = "hello world"
+# print("string: " + s)
+# print("reverse: " + s[::-1])
+# print("length: " + str(len(s)))
+
+l1 = [0,2,4,6]
+l2 = [0,1,3,5]
+
+line_segments = list(zip(l1, l2))
+
+for i, x1 in enumerate(line_segments):
+    print("idx: " + i.__str__() + " t1: " + str(x1[0]))
+    for x2 in line_segments[i + 1:]:
+        print("inner idx: " + i.__str__() + " t1: " + str(x2[1]))
