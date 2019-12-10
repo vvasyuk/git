476aa7bafdaaecfcd1cd3678a65e4ba2b55c5bd6
Vadym <vvasyuk@gmail.com>
2019-12-10 23:49:13 +0200

    upd

--- java/general/src/main/java/com/tryout/DailyCodingProblems/n194_line_intersection.scala
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/n194_line_intersection.scala
@@ -4,17 +4,28 @@ object n194_line_intersection {
 
   def execute(l1: Array[Int], l2: Array[Int]): Unit = {
 
-    for(n<-l1.zipWithIndex;
-        m<-l2.zipWithIndex if m._1 > n._1
-        ){
-      println(n._2 + " : " + m._2)
-    }
+    var cnt =0
+    val lineSegments = l1.zip(l2)
 
+    //crossing of 1st segment with second and third
+    //crossing of 2nd segment with third
+    for(n<-lineSegments.zipWithIndex;
+        m<-lineSegments.zipWithIndex if n._2 >m._2
+        ){
+      val p1 = n._1
+      val p2 = m._1
+      var inter=false
+
+      if((p1._1 > p2._1 && p1._2 < p2._2) || (p1._1 < p2._1 && p1._2 > p2._2)){
+        inter = true
+      }
+      println(p1 + " : " + p2 + " intersecting: " + inter)
+    }
   }
 
   def main(args: Array[String]):Unit={
-    val l1 = Array(1,3,5)
-    val l2 = Array(2,4,6)
+    val l1 = Array(1,2,5)
+    val l2 = Array(3,2,6)
     execute(l1, l2)
   }
 }
