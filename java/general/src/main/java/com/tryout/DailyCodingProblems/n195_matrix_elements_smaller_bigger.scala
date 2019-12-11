d64afc8d40738bb0e4f697f98528c2f4143c529a
Vadym <vvasyuk@gmail.com>
2019-12-12 01:17:26 +0200

    upd

--- java/general/src/main/java/com/tryout/DailyCodingProblems/n195_matrix_elements_smaller_bigger.scala
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/n195_matrix_elements_smaller_bigger.scala
@@ -2,22 +2,60 @@ package com.tryout.DailyCodingProblems
 
 object n195_matrix_elements_smaller_bigger {
 
-  def execute(arr: Array[Array[Int]], p1: (1, 1), p2: (3, 3)): Unit = {
-    val row = arr.size
-    val col = arr(0).size
+  def toRight(arr: Array[Array[Int]], row: Int, col:Int, value:Int):Int={
+    var cnt = 0
+    Range(0, row).foreach(x=>{
+      var y = col+1
+      while (x<arr(0).size && value>arr(x)(y)){
+        y+=1
+        cnt+=1
+      }
+    })
+    cnt
+  }
 
+  def toBottom(arr: Array[Array[Int]], row: Int, col:Int, value:Int):Int={
+    var cnt = 0
+    Range(0, col).foreach(y=>{
+      var x = row+1
+      while (x<arr.size && value>arr(x)(y)){
+        x+=1
+        cnt+=1
+      }
+    })
+    cnt
+  }
+
+  def execute(arr: Array[Array[Int]], p1: (Int, Int), p2: (Int, Int)): Unit = {
     var cnt = 0
 
     val a = arr(p1._1)(p1._2)
+    val b = arr(p2._1)(p2._2)
+    // cnt of smaller rectangle (to the top left from point p1)
+//    cnt += (p1._1+1)*(p1._2+1)
+//    cnt += toRight(arr, p1._1, p1._2, a)
+//    cnt += toBottom(arr, p1._1, p1._2, a)
 
-    var i = 0
-    var j = row
+    var x = 0
+    for (y <- Range(0, arr(0).size).reverse){
+      while (x<arr.size && a>arr(x)(y)){
+        println(arr(x)(y))
+        cnt+=y+1
+        x+=1
+      }
+    }
 
-    Range(0,j).reverse.foreach(x=>{
-      println(_)
-    })
+    //Count numbers greater than b
+    x = 0
+    for (y <- Range(0, arr(0).size).reverse){
+      while (x<arr.size && b>arr(x)(y)){
+        println(arr(x)(y))
+        cnt+=arr(0).size - (y+1)
+        x+=1
+      }
+    }
 
-
+    println("cnt: " + cnt)
   }
 
   def main(args: Array[String]):Unit={
@@ -31,29 +69,3 @@ object n195_matrix_elements_smaller_bigger {
     execute(arr, (1,1), (3,3))
   }
 }
-
-
-//i, j = 0, m - 1
-//for j in reversed(range(n)):
-//  while i < m and matrix[i][j] < a:
-//    i += 1
-//    count += i
-
-matrix = [[1, 3, 7, 10, 15, 20],
-[2, 6, 9, 14, 22, 25],
-[3, 8, 10, 15, 25, 30],
-[10, 11, 12, 23, 30, 35],
-[20, 25, 30, 35, 40, 45]]
-m, n = len(matrix), len(matrix[0])
-
-count = 0
-
-print(matrix[1][1])
-
-# Count numbers smaller than m[i1][j1]
-a = matrix[1][1]
-i, j = 0, m - 1
-for j in reversed(range(n)):
-while i < m and matrix[i][j] < a:
-print(matrix[i][j])
-i+=1
