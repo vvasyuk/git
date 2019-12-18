89906923499debd110556e93079654098b48d3c7
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2019-12-18 21:36:06 +0200

    upd

---
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/n204_btree_nodes_cnt.scala
@@ -0,0 +1,31 @@
+package com.tryout.DailyCodingProblems
+
+object n204_btree_nodes_cnt {
+
+  def main(args: Array[String]):Unit={
+    execute(Node(6, Node(5,Node(3),Node(4)),Node(9,Node(8))))
+  }
+
+  def execute(root: Node):Unit={
+    var left_cnt = 0
+    var right_cnt = 0
+    var tmp = root
+
+    while (tmp!=null){
+      left_cnt+=1
+      tmp=tmp.left
+    }
+
+    tmp=root
+    while (tmp!=null){
+      right_cnt+=1
+      tmp=tmp.right
+    }
+
+    if (left_cnt==right_cnt){
+      println((2<<left_cnt-1)-1)
+    }else{
+      println((2<<left_cnt-1)-2)
+    }
+  }
+}
---
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/n205_next_higher_permutation.scala
@@ -0,0 +1,40 @@
+package com.tryout.DailyCodingProblems
+
+object n205_next_higher_permutation {
+  def execute(arr: Array[Int]): Unit = {
+    var n = arr.size
+    var tailStart = n-1
+
+    while (tailStart!=0 && arr(tailStart-1) > arr(tailStart)){
+      tailStart-=1
+    }
+
+    if(tailStart==0) println("no such permutations")
+
+    var swap = tailStart
+    while(swap < n && arr(tailStart-1) < arr(swap)){
+      swap+=1
+    }
+    swap-=1
+
+    arr.foreach(print(_))
+    val tmp = arr(tailStart-1)
+    arr(tailStart-1)=arr(swap)
+    arr(swap)=tmp
+
+    while (tailStart<n-1){
+      val tmp = arr(tailStart)
+      arr(tailStart) = arr(n-1)
+      arr(n-1)=tmp
+      tailStart+=1
+      n-=1
+    }
+
+    println()
+    arr.foreach(print(_))
+  }
+
+  def main(args: Array[String]): Unit = {
+    execute(Array(4,8,9,7,5))
+  }
+}
