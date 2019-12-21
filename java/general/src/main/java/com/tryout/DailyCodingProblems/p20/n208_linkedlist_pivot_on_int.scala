94a889f95b591b7d2f9fc87bb2e29c5f87f75be6
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2019-12-21 21:23:58 +0200

    upd

--- java/general/src/main/java/com/tryout/DailyCodingProblems/p20/n208_linkedlist_pivot_on_int.scala
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/p20/n208_linkedlist_pivot_on_int.scala
@@ -4,6 +4,10 @@ import java.util
 
 import scala.collection.mutable.ArrayBuffer
 
+//Given a linked list of numbers and a pivot k, partition the linked list so that all nodes less than k come before nodes greater than or equal to k.
+//
+//For example, given the linked list 5 -> 1 -> 8 -> 0 -> 3 and k = 3, the solution could be 1 -> 0 -> 5 -> 8 -> 3.
+
 object n208_linkedlist_pivot_on_int {
 
   def execute(l: util.LinkedList[Int], k: 3): Unit = {
