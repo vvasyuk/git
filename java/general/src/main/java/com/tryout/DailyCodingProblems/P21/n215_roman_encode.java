7356ba62935df4c05df9220949fb61eb23138373
Vadym <vvasyuk@gmail.com>
2019-12-29 00:07:13 +0200

    upd

---
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/P21/n215_roman_encode.java
@@ -0,0 +1,38 @@
+package com.tryout.DailyCodingProblems.P21;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class n215_roman_encode {
+
+    public static void main(String[] args) {
+        execute("XIV");
+    }
+
+    static void execute(String s){
+        int sum = 0;
+        int prev = -1;
+        Map<String, Integer> m = new HashMap();
+        m.put("M", 1000);
+        m.put("D", 500);
+        m.put("C", 100);
+        m.put("L", 50);
+        m.put("X", 10);
+        m.put("V", 5);
+        m.put("I", 1);
+
+        for (char c : s.toCharArray()){
+            int curr = Integer.valueOf(m.get(String.valueOf(c)));
+            sum+=curr;
+            if(prev >0 && curr>prev){
+                sum=sum-2*prev;
+            }
+
+            prev=curr;
+            System.out.println(curr);
+        }
+        System.out.println("sum: " + sum);
+    }
+}
