df2bf3a80cceecee2627a8f9306d2082b359a1e7
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2019-12-29 21:54:51 +0200

    upd

---
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/P21/n215_horizontal_dist.java
@@ -0,0 +1,51 @@
+package com.tryout.DailyCodingProblems.P21;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//The horizontal distance of a binary tree node describes how far left or right the node will be when the tree is printed out.
+//More rigorously, we can define it as follows:
+//
+//The horizontal distance of the root is 0.
+//The horizontal distance of a left child is hd(parent) - 1.
+//The horizontal distance of a right child is hd(parent) + 1.
+//For example, for the following tree, hd(1) = -2, and hd(6) = 0.
+//
+//            5
+//         /     \
+//        3        7
+//      /   \    /   \
+//     1     4  6     9
+//    /              /
+//   0              8
+//The bottom view of a tree, then, consists of the lowest node at each horizontal distance. If there are two nodes at the same depth and horizontal distance, either is acceptable.
+//
+//For this tree, for example, the bottom view could be [0, 1, 3, 6, 8, 9].
+//
+//Given the root to a binary tree, return its bottom view.
+
+public class n215_horizontal_dist {
+    public static void main(String[] args) {
+        Node root = new Node(5,
+                new Node(3, new Node(1, new Node(0), null), new Node(4)),
+                new Node(7,new Node(6),new Node(9,new Node(8), null))
+        );
+
+    }
+
+    static class Node{
+        Integer value;
+        Node left;
+        Node right;
+
+        public Node(Integer value, Node left, Node right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+
+        public Node(Integer value) {
+            this.value = value;
+        }
+    }
+}
