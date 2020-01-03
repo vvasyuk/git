b31403eb2f82bb10a0d4cf6e81a70ff166f0b06e
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2020-01-03 12:02:43 +0200

    upd

---
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/P22/n221_sevenish_numbers.java
@@ -0,0 +1,11 @@
+package com.tryout.DailyCodingProblems.P22;
+
+//Let's define a "sevenish" number to be one which is either a power of 7, or the sum of unique powers of 7.
+//The first few sevenish numbers are 1, 7, 8, 49, and so on. Create an algorithm to find the nth sevenish number.
+
+public class n221_sevenish_numbers {
+
+    public static void main(String[] args) {
+        
+    }
+}
