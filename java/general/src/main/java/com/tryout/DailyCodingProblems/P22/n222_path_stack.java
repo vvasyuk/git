4994f647b3884890baf2f7248db921831b033dfe
Vadym <vvasyuk@gmail.com>
2020-01-04 23:28:08 +0200

    upd

--- java/general/src/main/java/com/tryout/DailyCodingProblems/P22/n222_path_stack.java
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/P22/n222_path_stack.java
@@ -3,10 +3,25 @@ package com.tryout.DailyCodingProblems.P22;
 //Given an absolute pathname that may have . or .. as part of it, return the shortest standardized path.
 //For example, given "/usr/bin/../bin/./scripts/../", return "/usr/bin/".
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class n222_path_stack {
 
     public static void main(String[] args) {
-        String path = "/usr/bin/../bin/./scripts/../";
+        String path = "/usr/bin/../bin/./scripts/../";  // /usr/bin/
+        LinkedList<String> res = new LinkedList<String>();
         System.out.println(path);
+
+        String[] splitted = path.split("/");
+
+        Arrays.stream(splitted).forEach(x-> {
+            if(x.equals("..")){
+                res.removeLast();
+            }else if (!x.equals(".")){
+                res.addLast(x);
+            }
+        });
+        res.forEach(x-> System.out.print(x));
     }
 }
