c151aebb92f0ab1eefd642b7ad28b6994b1317ec
Vadym <vvasyuk@gmail.com>
2020-01-12 20:52:36 +0200

    upd

---
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/p23/n231_rearrange_repeated_chars.java
@@ -0,0 +1,7 @@
+package com.tryout.DailyCodingProblems.p23;
+
+// Given a string with repeated characters, rearrange the string so that no two adjacent characters are the same.
+// If this is not possible, return None.
+// For example, given "aaabbc", you could return "ababac". Given "aaab", return None.
+public class n231_rearrange_repeated_chars {
+}
