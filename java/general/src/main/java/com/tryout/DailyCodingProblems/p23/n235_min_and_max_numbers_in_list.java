31121d0bc9319de54ecc95aa7a08ff62c7d4987d
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2020-01-19 14:28:06 +0200

    upd

--- java/general/src/main/java/com/tryout/DailyCodingProblems/p23/n235_min_and_max_numbers_in_list.java
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/p23/n235_min_and_max_numbers_in_list.java
@@ -2,8 +2,13 @@ package com.tryout.DailyCodingProblems.p23;
 
 // Given an array of numbers of length N, find both the minimum and maximum using less than 2 * (N - 2) comparisons.
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class n235_min_and_max_numbers_in_list {
     public static void main(String[] args) {
-        
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(4, 2, 7, 5, -1, 3, 6));
+
     }
 }
