af6fbb1d6864873e9e47bd255788eb484b45e45b
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2020-01-31 21:03:47 +0200

    upd

---
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/p25/n250_letter_is_digit.java
@@ -0,0 +1,13 @@
+package com.tryout.DailyCodingProblems.p25;
+
+// A cryptarithmetic puzzle is a mathematical game where the digits of some numbers are represented by letters. Each letter represents a unique digit.
+
+//   SEND
+// + MORE
+//--------
+//  MONEY
+//may have the solution:
+//
+//{'S': 9, 'E': 5, 'N': 6, 'D': 7, 'M': 1, 'O', 0, 'R': 8, 'Y': 2}
+public class n250_letter_is_digit {
+}
