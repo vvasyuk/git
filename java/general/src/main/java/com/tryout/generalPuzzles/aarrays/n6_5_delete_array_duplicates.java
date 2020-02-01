f91838ce00761e3d07b8e08e1f39216c6a578f09
Vadym <vvasyuk@gmail.com>
2020-02-01 23:05:35 +0200

    upd

--- java/general/src/main/java/com/tryout/generalPuzzles/aarrays/n6_5_delete_array_duplicates.java
+++ java/general/src/main/java/com/tryout/generalPuzzles/aarrays/n6_5_delete_array_duplicates.java
@@ -1,9 +1,16 @@
 package com.tryout.generalPuzzles.aarrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class n6_5_delete_array_duplicates {
     public static void main(String[] args) {
+        int[] in = new int[] {2,3,5,5,7,11,11,11,13};
+        ArrayList l = new ArrayList(Arrays.asList(2, 3, 5, 5, 7, 11, 11, 11, 13));
+        deleteDuplicates(l);
+
+        l.forEach(x-> System.out.println(x));
 
     }
 
