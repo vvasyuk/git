c31414ea0469b7722eef3fe251f25a3c0cb8f656
Vadym <vvasyuk@gmail.com>
2020-02-03 22:56:44 +0200

    UPD

--- java/general/src/main/java/com/tryout/DailyCodingProblems/p25/n252_egyptian_fractions.java
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/p25/n252_egyptian_fractions.java
@@ -2,14 +2,17 @@ package com.tryout.DailyCodingProblems.p25;
 
 import java.util.ArrayList;
 
-// The ancient Egyptians used to express fractions as a sum of several terms where each numerator is one. For example, 4 / 13 can be represented as 1 / 4 + 1 / 18 + 1 / 468.
+// The ancient Egyptians used to express fractions as a sum of several terms where each numerator is one.
+// For example, 4 / 13 can be represented as 1 / 4 + 1 / 18 + 1 / 468.
 //
 //Create an algorithm to turn an ordinary fraction a / b, where a < b, into an Egyptian fraction.
 public class n252_egyptian_fractions {
     // we can directly find the next denominator to be the smallest integer above b / a.
-    // This works because (a / b) is equal to 1 / (b / a), which must be greater than 1 / ceil(b / a). For example, for 4 / 13, the first denominator should be ceil(13 / 4) = 4.
+    // This works because (a / b) is equal to 1 / (b / a), which must be greater than 1 / ceil(b / a).
+    // For example, for 4 / 13, the first denominator should be ceil(13 / 4) = 4.
     //
-    //In addition, we can simplify the calculation of our new numerator by noting that since c = 1 and we have updated d, a * d - b * c can be changed to (-b) % a.
+    //In addition, we can simplify the calculation of our new numerator by noting that since c = 1
+    // and we have updated d, a * d - b * c can be changed to (-b) % a.
     public static void main(String[] args) {
         fractions(4.0,13.0);
     }
--- java/general/src/main/java/com/tryout/generalPuzzles/aarrays/n6_7_buy_stock_twice.java
+++ java/general/src/main/java/com/tryout/generalPuzzles/aarrays/n6_7_buy_stock_twice.java
@@ -8,22 +8,23 @@ public class n6_7_buy_stock_twice {
     public static double buyAndSellStockTwice(List<Double> prices) {
         double maxTotalProfit = 0.0;
         List<Double> firstBuySellProfits = new ArrayList<>();
-        double minPriceSoFar = Double.MAX_VALUE ;
-// Forward phase. For each day, we record maximum profit if we
-// sell on that day.
+        double minPriceSoFar = Double.MAX_VALUE;
+        // Forward phase. For each day, we record maximum profit if we
+        // sell on that day.
         for (int i = 0; i < prices.size(); ++i) {
-            minPriceSoFar = Math.min(minPriceSoFar , prices.get(i));
-            maxTotalProfit = Math.max(maxTotalProfit , prices.get(i) - minPriceSoFar);
+            minPriceSoFar = Math.min(minPriceSoFar, prices.get(i));
+            maxTotalProfit = Math.max(maxTotalProfit, prices.get(i) - minPriceSoFar);
             firstBuySellProfits.add(maxTotalProfit);
         }
-// Backward phase. For each day, find the maximum profit if we make
-// the second buy on that day.
-        double maxPriceSoFar = Double.MIN_VALUE ;
+        // Backward phase. For each day, find the maximum profit if we make
+        // the second buy on that day.
+        double maxPriceSoFar = Double.MIN_VALUE;
         for (int i = prices.size() - 1; i > 0; --i) {
-            maxPriceSoFar = Math.max(maxPriceSoFar , prices.get(i));
+            maxPriceSoFar = Math.max(maxPriceSoFar, prices.get(i));
             maxTotalProfit
-                    = Math.max(maxTotalProfit , maxPriceSoFar - prices.get(i)
+                    = Math.max(maxTotalProfit, maxPriceSoFar - prices.get(i)
                     + firstBuySellProfits.get(i - 1));
         }
-        return maxTotalProfit ;}
+        return maxTotalProfit;
+    }
 }
