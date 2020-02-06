b6bba46ff073c955cafd8e61da8cb79280e9d33e
Vadym <vvasyuk@gmail.com>
2020-02-06 22:58:18 +0200

    upd

--- java/general/src/main/java/com/tryout/DailyCodingProblems/p25/n255_graph_transitive_closure.java
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/p25/n255_graph_transitive_closure.java
@@ -48,28 +48,16 @@ public class n255_graph_transitive_closure {
             for (j = 0; j < V; j++)
                 reach[i][j] = graph[i][j];
 
-        /* Add all vertices one by one to the set of intermediate
-           vertices.
-          ---> Before start of a iteration, we have reachability
-               values for all  pairs of vertices such that the
-               reachability values consider only the vertices in
-               set {0, 1, 2, .. k-1} as intermediate vertices.
-          ----> After the end of a iteration, vertex no. k is
-                added to the set of intermediate vertices and the
-                set becomes {0, 1, 2, .. k} */
-        for (k = 0; k < V; k++)
-        {
-            // Pick all vertices as source one by one
-            for (i = 0; i < V; i++)
-            {
-                // Pick all vertices as destination for the
-                // above picked source
-                for (j = 0; j < V; j++)
-                {
+
+        // go through all combinations abut in addition go through another V (k) and check if
+        // any of V also connect to i or j
+        for (k = 0; k < V; k++) {
+            for (i = 0; i < V; i++) {
+                for (j = 0; j < V; j++) {
                     // If vertex k is on a path from i to j,
                     // then make sure that the value of reach[i][j] is 1
-                    reach[i][j] = (reach[i][j]!=0) ||
-                            ((reach[i][k]!=0) && (reach[k][j]!=0))?1:0;
+                    reach[i][j] =
+                            (reach[i][j]!=0) || ((reach[i][k]!=0) && (reach[k][j]!=0))?1:0;
                 }
             }
         }
