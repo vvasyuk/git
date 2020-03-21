f8169723c29e29d7088ed97129c7a6bcb944c2b0
Vadym <vvasyuk@gmail.com>
2020-03-22 00:16:58 +0200

    upd

--- java/general/src/main/java/com/tryout/DailyCodingProblems/p27/n274_eval_string.scala
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/p27/n274_eval_string.scala
@@ -5,40 +5,34 @@ import scala.collection.mutable
 object n274_eval_string {
 
   def main(args: Array[String]): Unit = {
-    val in = List("1", "-", "(", "2", "+", "3", ")")
+    val in = List("1", "-", "(", "2", "+", "3", ")", "-", "1")
     val ops = mutable.Stack[Char]()
     val values = mutable.Stack[Int]()
 
     parse(in, ops, values)
-    eval(ops, values)
     println(values.pop())
   }
 
   def parse(in: List[String], ops: mutable.Stack[Char], values: mutable.Stack[Int]): Unit = {
     in.mkString("").toCharArray.foreach(c=>c match{
       case i if i.isDigit => values.push(i.asDigit)
-      case '+' => ops.push(c)
-      case '-' => ops.push(c)
       case '(' => ops.push(c)
-      case ')' => ops.push(c)
-      case _ =>
-    })
-  }
-  // 1+(1+1)
-  // 1+(1+1)+1
-  def eval(ops: mutable.Stack[Char], values: mutable.Stack[Int]) = {
-    while(ops.nonEmpty){
-      var op = ops.pop()
-      if (op == ')') {
+      case ')' => {
         ops.popWhile(_ != '(').foreach(x => {
           oneOp(values, x)
         })
         ops.pop()
-      }else{
-        oneOp(values, op)
       }
+      case _ => {
+        ops.popWhile(_ != '(').foreach(x => {
+        oneOp(values, x)
+      })
+        ops.push(c)
+      }
+    })
+
+    ops.popAll().foreach(op=>oneOp(values, op))
   }
-}
 
   def oneOp(values: mutable.Stack[Int], op: Char)={
     val v1 = values.pop()
