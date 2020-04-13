a583a05c615b36f93abbafb60ffb4ab1d81f4f24
Vadym <vvasyuk@gmail.com>
2020-04-14 00:07:20 +0300

    upd

--- java/general/src/main/java/com/tryout/DailyCodingProblems/p29/p296_sorted_to_btree.scala
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/p29/p296_sorted_to_btree.scala
@@ -1,5 +1,7 @@
 package com.tryout.DailyCodingProblems.p29
 
+import scala.collection.mutable.ArrayBuffer
+
 // Given a sorted array, convert it into a height-balanced binary search tree.
 object p296_sorted_to_btree {
 
@@ -16,8 +18,34 @@ object p296_sorted_to_btree {
 
   }
 
+
+
+  def pr(root: Node):Unit = {
+    def _getMaxDepth(root: Node, d: Int):Int = root match{
+      case null => d
+      case _ => {
+        val l = _getMaxDepth(root.l, d+1)
+        val r = _getMaxDepth(root.r, d+1)
+        math.max(l,r)
+      }
+    }
+    val maxDepth = _getMaxDepth(root, 1)-1
+    val res = Array.fill(maxDepth)(ArrayBuffer[String]())
+
+    def _buildArray(root: Node, level: Int, col: Int, res: Array[ArrayBuffer[String]]): Unit = {
+      if (root==null) return
+      val prefix = if (col==0) math.pow(2,level-1)-1 else math.pow(2,level)-1
+      res(level-1)+=" " * prefix.toInt + root.v.toString
+      _buildArray(root.l,level-1,col,res)
+      _buildArray(root.r,level-1,col+1,res)
+    }
+    _buildArray(root,maxDepth,0,res)
+    res.reverse.foreach(x=>println(x.mkString("")))
+    println("")
+  }
+
   def main(args: Array[String]): Unit = {
-    val in = Array(1,2,3,4,5)
+    val in = Array(1,2,3,4,5,6,7)
 //    val m = in.size/2
 //    val l = in.take(m)
 //    val r = in.takeRight(in.size-m-1)
@@ -25,7 +53,9 @@ object p296_sorted_to_btree {
 
     val res = constrBTree(in)
 
+    pr(res)
     println("x")
+
   }
 }
 
