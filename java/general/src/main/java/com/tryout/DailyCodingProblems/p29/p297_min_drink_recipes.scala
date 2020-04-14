77e7a1354dddc52829025b5be4761ef109d25275
Vadym <vvasyuk@gmail.com>
2020-04-15 00:28:23 +0300

    upd

--- java/general/src/main/java/com/tryout/DailyCodingProblems/p29/p297_min_drink_recipes.scala
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/p29/p297_min_drink_recipes.scala
@@ -14,14 +14,48 @@ package com.tryout.DailyCodingProblems.p29
 //
 //For the input above, the answer would be 2, as drinks 1 and 5 will satisfy everyone.
 object p297_min_drink_recipes {
+
+  def satisfy(c: List[Int], in: Map[Int, List[Int]]): Boolean = {
+    val values = in.values.toList
+    values.forall(x=>{
+      val t = c.toSet.intersect(x.toSet)
+      t.size > 0
+    })
+  }
+
+
   def main(args: Array[String]): Unit = {
     val in = Map(
-      0 -> List (0,1,3,6),
+      0 -> List (0, 1, 3, 6),
       1 -> List (1, 4, 7),
       2 -> List (2, 4, 7, 5),
       3 -> List (3, 2, 5),
       4 -> List (5, 8),
     )
+//    val ol = List(None,None,Some(1),Some(2)).view
+//    val r = ol.find(_.isDefined).flatten
+//    print("")
+
+//    val a = List(Set(1,2,3),Set(4,5,6),Set(7,8,9))
+//    val b = Set(1,5,9)
+//    val x = a.forall(x=>{
+//      val r = x.intersect(b)
+//      r.size>0
+//    })
+//    print(x)
+
+    val customers = in.keySet
+    val drinks = (in.values.flatten)
+
+    val res = (for(i<- (1 to 3).view;
+        combs <- drinks.toList.sorted.combinations(i)
+    )yield{
+      if (satisfy(combs,in)){
+        println("res: " + i + " combination: " + combs.mkString(","))
+        Some(combs)
+      }  else None
+    }).find(_.isDefined).flatten.get
+    println("res: " + res)
   }
 }
 
