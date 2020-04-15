c7f3aee5b06647a2a904a3502ebc8633d5b5663a
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2020-04-15 21:05:46 +0300

    upd

---
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/p29/p298_apple_trees.scala
@@ -0,0 +1,18 @@
+package com.tryout.DailyCodingProblems.p29
+
+// A girl is walking along an apple orchard with a bag in each hand. She likes to pick apples from each tree as she goes along, but is meticulous about not putting different kinds of apples in the same bag.
+//
+//Given an input describing the types of apples she will pass on her path, in order, determine the length of the longest portion of her path that consists of just two types of apple trees.
+//
+//For example, given the input [2, 1, 2, 3, 3, 1, 3, 5], the longest portion will involve types 1 and 3, with a length of four.
+object p298_apple_trees {
+
+  def execute(in: Array[Int]): Int = {
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    val in = Array(2, 1, 2, 3, 3, 1, 3, 5)
+    execute(in)
+  }
+}
--- sbt/spartTest/src/main/scala/com/tryout/spark/SparkTest.scala
+++ sbt/spartTest/src/main/scala/com/tryout/spark/SparkTest.scala
@@ -1,12 +1,24 @@
 package com.tryout.spark
 
+import java.io.File
+
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 import resource._
+
 
 object SparkTest {
   def main(args: Array[String]): Unit = {
     println("SparkTest")
-
+    getListOfFiles("D:\\work\\tryout\\sbt\\data\\in").foreach(println(_))
+    val dirs = new File("D:\\work\\tryout\\sbt\\data").listFiles().filter(_.isDirectory)
+    val files = dirs.flatMap(x=>{
+      if(x.isFile){
+        println(x.getName)
+        x.getName
+      }
+      ""
+    })
 
 //    spark.catalog.listDatabases.show(false)
 //    spark.catalog.listTables.show(false)
@@ -14,7 +26,7 @@ object SparkTest {
     //val spark = getSession
     //dummyJob1(spark)
 
-    managed(getSession).acquireAndGet(spark => dummyJob3(spark))
+    //managed(getSession).acquireAndGet(spark => dummyJob3(spark))
 //    for {
 //      spark <- managed(getSession)
 //    } {
@@ -42,18 +54,48 @@ object SparkTest {
     .getOrCreate()
 
   def dummyJob3(spark: SparkSession):Unit={
+    import spark.implicits._
     // read one file
     //val df = spark.read.option("header", "true").csv("D:\\work\\tryout\\sbt\\data\\in\\a.csv")
-
-    // list of filenames
-    val paths = List("D:\\work\\tryout\\sbt\\data\\in\\a.csv", "D:\\work\\tryout\\sbt\\data\\in\\b.csv", "D:\\work\\tryout\\sbt\\data\\in\\c.csv")
-    val df = spark.read.option("header", "true").csv(paths: _*)
 
     // read from multiple folders
     //val folders = List("D:\\work\\tryout\\sbt\\data\\in")         // "dir1", "dir2"
     //val df = spark.read.option("header", "true").csv(folders: _*)
 
-    df.show(15)
+    // list of filenames
+    val paths = List("D:\\work\\tryout\\sbt\\data\\in\\a.csv", "D:\\work\\tryout\\sbt\\data\\in\\b.csv", "D:\\work\\tryout\\sbt\\data\\in\\c.csv")
+    val df = spark.read.option("header", "true").csv(paths: _*)
+    val df1 = df.withColumn("file", substring_index(input_file_name(), "/", -1))
+    val df2 = List("c.csv").toDF("file")
+
+    val cols1 = df1.columns.toSet
+    val cols2 = df2.columns.toSet
+    val total = cols1 ++ cols2 // union
+
+    val order = df1.columns ++  df2.columns
+    val sorted = total.toList.sortWith((a,b)=> order.indexOf(a) < order.indexOf(b))
+
+    def expr(myCols: Set[String], allCols: List[String]) = {
+      allCols.map( {
+        case x if myCols.contains(x) => col(x)
+        case y => lit(null).as(y)
+      })
+    }
+
+    val df3 = df1.select(expr(cols1, sorted): _*).unionAll(df2.select(expr(cols2, sorted): _*))
+
+    df1.show(15)
+    df2.show(15)
+    df3.show(15)
+  }
+
+  def getListOfFiles(dir: String):List[File] = {
+    val d = new File(dir)
+    if (d.exists && d.isDirectory) {
+      d.listFiles.filter(_.isFile).toList
+    } else {
+      List[File]()
+    }
   }
 }
 
