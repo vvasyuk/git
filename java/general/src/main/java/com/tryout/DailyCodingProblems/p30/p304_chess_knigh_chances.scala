694b4052497c7f308929dc0d52bba67836002b56
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2020-04-19 21:26:09 +0300

    upd

--- java/general/src/main/java/com/tryout/DailyCodingProblems/p30/p304_chess_knigh_chances.scala
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/p30/p304_chess_knigh_chances.scala
@@ -1,5 +1,7 @@
 package com.tryout.DailyCodingProblems.p30
 
+import scala.collection.mutable
+
 object p304_chess_knigh_chances {
   def getMoves(x: Int, y: Int) = {
     val moves = Array((1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-2, 1), (-1, 2))
@@ -14,25 +16,33 @@ object p304_chess_knigh_chances {
     x>=0 && x<=7 && y>=0 && y<=7
   }
 
+  def getProbability(x: Int, y: Int, k: Int, memo: mutable.Map[(Int, Int, Int), Double]): Double = {
+    if(memo.contains(x,y,k)){
+      return memo(x,y,k)
+    }
+
+    if(k==0){
+      return if (onBoard(x,y)) 1 else 0
+    }
+    if(!onBoard(x,y)){
+      return 0
+    }
+    val jumps =  getMoves(x,y)
+    val res = for(j<-jumps)yield{
+      val (x,y) = j
+      getProbability(x,y,k-1,memo)
+    }
+    memo.put((x,y,k), 0.125 * res.sum)
+    memo(x,y,k)
+  }
+
   def main(args: Array[String]): Unit = {
 
     val moves = getMoves(0,0)
     moves.foreach(pos=>println("pos: " + pos + " on board: " + onBoard(pos)))
+
+    val memo = mutable.Map[(Int,Int,Int),Double]()
+    println(getProbability(0,0,1,memo))
+
   }
 }
-// bottom-up dynamic programming
-// def get_probability(x, y, k, memo={}):
-//    if (x, y, k) in memo:
-//        return memo[(x, y, k)]
-//
-//    if k == 0:
-//        return on_board(x, y)
-//    if not on_board(x, y):
-//        return 0
-//
-//    jumps = get_moves(x, y)
-//    probs = [get_probability(x, y, k - 1, memo) for x, y in jumps]
-//
-//    memo[(x, y, k)] = 0.125 * sum(probs)
-//
-//    return memo[(x, y, k)]
