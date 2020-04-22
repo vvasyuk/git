b83b70abd898d46b168ee76578d01c8dee9280f7
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2020-04-22 22:26:46 +0300

    upd

--- python/python3/learn/test.py
+++ python/python3/learn/test.py
@@ -1,4 +1,30 @@
-from itertools import product
-l=list(product((0, 3), range(5 + 1)))
-l2 = list(product(range(1, 3), (0, 5)))
-print(l2)
+from collections import deque
+
+def unlock(start, target, deadends):
+    queue = deque([start])
+    visited = set(deadends)
+    level = 0
+
+    while queue:
+        for _ in range(len(queue)):
+            curr = queue.popleft()
+            if curr == target:
+                return level
+
+            else:
+                for i, wheel in enumerate(curr):
+                    lower = curr[:i] + str((int(wheel) - 1) % 10) + curr[i + 1:]
+                    higher = curr[:i] + str((int(wheel) + 1) % 10) + curr[i + 1:]
+
+                    if lower not in visited:
+                        visited.add(lower)
+                        queue.append(lower)
+                    if higher not in visited:
+                        visited.add(higher)
+                        queue.append(higher)
+
+        level += 1
+
+    return None
+
+print(unlock("011","555","999"))
