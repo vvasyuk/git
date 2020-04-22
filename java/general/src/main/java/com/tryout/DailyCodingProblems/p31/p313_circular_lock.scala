aa1fc374de3463a44808554fca335e58bdf4945c
Vadym <vvasyuk@gmail.com>
2020-04-23 00:30:33 +0300

    upd

--- java/general/src/main/java/com/tryout/DailyCodingProblems/p31/p313_circular_lock.scala
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/p31/p313_circular_lock.scala
@@ -1,5 +1,7 @@
 package com.tryout.DailyCodingProblems.p31
 
+import scala.collection.mutable
+
 // You are given a circular lock with three wheels, each of which display the numbers 0 through 9 in order. Each of these
 // wheels rotate clockwise and counterclockwise.
 //
@@ -10,9 +12,46 @@ package com.tryout.DailyCodingProblems.p31
 // a target combination, and a list of dead ends, write a function that returns the minimum number of moves required to reach the
 // target state, or None if this is impossible.
 
+// TODO: make it more functional
 object p313_circular_lock {
   def main(args: Array[String]): Unit = {
-    
-    unlock(000, 555)
+    println(unlock("123", "133"))
   }
+
+  def unlock(start:String, target: String): Int ={
+    val q = mutable.Queue[String]()
+    val visited = mutable.Set[String]()
+    var level = 0
+    var found = false
+    q.enqueue(start)
+
+    while(q.nonEmpty){
+      for(curr <- (q.dequeueAll(_=>true)) if (!found)){
+        if (curr==target){
+          found=true
+          return level
+        }
+        for(i<-0 until curr.size){
+          val tlower = (((curr(i).toString.toInt)-1)%10).toString
+          val thigher = (((curr(i).toString.toInt)+1)%10).toString
+
+          val lower = curr.updated(i,tlower(0))
+          val higher = curr.updated(i,thigher(0))
+          println("level: " + level + " lower:" + lower + " higher: " + higher)
+
+          if (!visited.contains(lower)){
+            visited+=lower
+            q.enqueue(lower)
+          }
+          if (!visited.contains(higher)){
+            visited+=higher
+            q.enqueue(higher)
+          }
+        }
+      }
+      level+=1
+    }
+    level
+  }
+
 }
--- java/general/src/main/java/com/tryout/DailyCodingProblems/test.scala
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/test.scala
@@ -2,8 +2,9 @@ package com.tryout.DailyCodingProblems
 
 object test {
   def main(args: Array[String]): Unit = {
-    val p = "/1/2/3/4/5/6"
-    val i  = p.substring(p.substring(0, p.lastIndexOf("/")).lastIndexOf("/"));
-    println(i)
+    val a = (0%10).toString
+
+    println(a(0))
+    println('0')
   }
 }
