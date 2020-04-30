e5122de4c21527d5f3becb38a3f646e74e416d9f
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2020-04-30 22:05:44 +0300

    upd

--- java/general/src/main/java/com/tryout/DailyCodingProblems/p32/p322_jump_series_on_line.scala
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/p32/p322_jump_series_on_line.scala
@@ -7,4 +7,25 @@ package com.tryout.DailyCodingProblems.p32
 //Find a path with the fewest number of jumps required to get from 0 to N.
 object p322_jump_series_on_line {
 
+  def main(args: Array[String]): Unit = {
+    val N = 11
+    
+    jumpPath(N)
+  }
+
+  def jumpPath(N: Int): Unit = {
+    var n = N
+    var isMegative = false
+    if (n<0){
+      n = -n
+      isMegative = true
+    }
+
+    var (k,total) = (0,0)
+    while(total<0 || (total >0 && (total-n)%2!=0)){
+      k+=1
+      total+=k
+    }
+
+  }
 }
