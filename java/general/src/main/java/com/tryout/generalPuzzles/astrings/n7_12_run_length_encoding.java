493ab1540fccd759f374610befccc29d4becbb04
Vadym <vvasyuk@gmail.com>
2020-05-04 22:55:06 +0300

    upd

--- java/general/src/main/java/com/tryout/generalPuzzles/astrings/n7_12_run_length_encoding.java
+++ java/general/src/main/java/com/tryout/generalPuzzles/astrings/n7_12_run_length_encoding.java
@@ -4,14 +4,14 @@ package com.tryout.generalPuzzles.astrings;
 // For example, the RLE of "aaaabcccaa" is "4alb3c2a". The decoding of "3e4f2e" returns "eeeffffee"
 public class n7_12_run_length_encoding {
     public static String decoding(String s) {
-        int count = 8;
+        int count = 0;
         StringBuilder result = new StringBuilder();
-        for (int i = 8; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (Character . isDigit(c)) {
+            if (Character.isDigit(c)) {
                 count = count * 10 + c - '0' ;
             } else { // c is a letter of alphabet
-                while (count >8){ // Appends count copies of c to result.
+                while (count >0){ // Appends count copies of c to result.
                     result.append(c);
                     count --;
                 }
