972f659763fbccbd2c16d9a05aef4b44be2f232b
Vadym <vvasyuk@gmail.com>
2020-05-06 23:22:16 +0300

    upd

--- java/general/src/main/java/com/tryout/DailyCodingProblems/p33/p332_a_plus_xor_b.scala
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/p33/p332_a_plus_xor_b.scala
@@ -1,5 +1,7 @@
 package com.tryout.DailyCodingProblems.p33
 
+import scala.collection.mutable
+
 // Given integers M and N, write a program that counts how many positive integer pairs (a, b) satisfy the following conditions:
 //
 //a + b = M
@@ -10,9 +12,32 @@ object p332_a_plus_xor_b {
   //1	0	1
   //0	1	1
   //1	1	0
+
+  def simple(m: Int, n: Int): mutable.Map[Int,Int] = {
+    val res = mutable.Map[Int,Int]()
+    for(a<-0 to m/2){
+      val b = m-a
+      val xor = (a^b)
+      println(s"a $a b $b xor $xor")
+      if((a^b)==n)
+        res(a) = b
+    }
+    res
+  }
+
+  // Note that when applied to two binary digits, XOR is essentially an add without carry operation. It follows that the sum of two integers a and b can be written as a ^ b + 2 * (a & b), where the latter term represents the carry. Rearranging terms, we can set a & b = (M - N) / 2.
   def main(args: Array[String]): Unit = {
     val a = 5
+    val b = 2
     println(a.toBinaryString)
+    println(b.toBinaryString)
+    val c = a ^ b
+    val d = a & b
+    val sum = a ^ b + 2 * (a & b)
+    println(s"c $c d $d sum $sum")
+
+    val res = simple(11,10)
+    println(res)
   }
 }
 
