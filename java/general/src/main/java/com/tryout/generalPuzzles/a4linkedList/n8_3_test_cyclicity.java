d15fbde8de119f514180aacef6f78e3f600ffa43
Vadym <vvasyuk@gmail.com>
2020-05-08 23:00:38 +0300

    upd

--- java/general/src/main/java/com/tryout/generalPuzzles/a4linkedList/n8_3_test_cyclicity.java
+++ java/general/src/main/java/com/tryout/generalPuzzles/a4linkedList/n8_3_test_cyclicity.java
@@ -1,6 +1,11 @@
 package com.tryout.generalPuzzles.a4linkedList;
 
 public class n8_3_test_cyclicity {
+    //slow iterator and a fast iterator
+    //to traverse the list. In each iteration, advance the slow iterator by one and the fast
+    //iterator by two. The list has a cycle if and only if the two iterators meet. The reasoning
+    //is as follows: if the fast iterator jumps over the slow iterator, the slow iterator will
+    //equal the fast iterator in the nextstep.
     public static ListNode<Integer> hasCycle(ListNode<Integer> head) {
         ListNode<Integer> fast = head, slow = head;
         while (fast != null && fast.next != null) {
