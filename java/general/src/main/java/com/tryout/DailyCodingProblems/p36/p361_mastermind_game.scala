153f4563b3f4cff4e9adb2f177bf35b1441693fd
Vadym <vvasyuk@gmail.com>
2020-05-19 23:34:09 +0300

    upd

--- java/general/src/main/java/com/tryout/DailyCodingProblems/p36/p361_mastermind_game.scala
+++ java/general/src/main/java/com/tryout/DailyCodingProblems/p36/p361_mastermind_game.scala
@@ -19,13 +19,9 @@ import scala.collection.mutable.ArrayBuffer
 // {123456: 4, 345678: 4, 567890: 4}
 object p361_mastermind_game {
   // First note that after each guess, we can narrow down the set of possible codes to those that match the corresponding score. This way, each subsequent guess will take less time to evaluate.
-  //
   //Second, we can begin our pool of possible codes with only those that satisfy our most restrictive guess. Instead of iterating through every code to see whether it satisfies this guess, though, we can do this constructively.
-  //
   //For example, suppose our guess is 123456 and its score is five. Then only one of the indices is incorrect, and it must be replaced by some other digit. These replacement digits, however, must not duplicate an existing digit. Therefore, we end up restricting our set of possible codes to the following:
-  //
   //[[789]23456, 1[789]3456, 12[789]456, 123[789]56, 1234[789]6, 12345[789]]
-  //
   //In other words, using the first guess, we have narrowed down the pool of possible solutions from roughly 105 to 36.
 
 
@@ -43,7 +39,7 @@ object p361_mastermind_game {
           digits.append(x)
         }
       }
-      
+
 
     }
 
