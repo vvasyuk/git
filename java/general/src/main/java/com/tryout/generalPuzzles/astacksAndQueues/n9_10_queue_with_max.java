2231d8dc6f30ff8a18327998c33a44ee69fa9c0e
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2020-05-28 23:07:15 +0300

    upd

---
+++ java/general/src/main/java/com/tryout/generalPuzzles/astacksAndQueues/n9_10_queue_with_max.java
@@ -0,0 +1,54 @@
+package com.tryout.generalPuzzles.astacksAndQueues;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+
+// Implement a queue with enqueue, dequeue, and max operations. The max operation
+//returns the maximum element currently stored in the queue.
+public class n9_10_queue_with_max {
+
+    public static void main(String[] args) {
+        Deque<Integer> entries = new LinkedList<>();
+        entries.add(1);
+        entries.add(2);
+        System.out.println(entries.getLast());
+
+    }
+
+    public static class QueueWithMax<T extends Comparable<T>> {
+        private Queue<T> entries = new LinkedList<>();
+        private Deque<T> candidatesForMax = new LinkedList<>();
+
+        public void enqueue(T x) {
+            entries.add(x);
+            while (!candidatesForMax.isEmpty()) {
+                // Eliminate dominated elements in candidatesForMax.
+                if (candidatesForMax.getLast().compareTo(x) >= 0) {
+                    break;
+                }
+                candidatesForMax.removeLast();
+            }
+            candidatesForMax.addLast(x);
+        }
+
+        public T dequeue() {
+            if (!entries.isEmpty()) {
+                T result = entries.remove();
+                if (result.equals(candidatesForMax.getFirst())) {
+                    candidatesForMax.removeFirst();
+                }
+                return result;
+            }
+            throw new NoSuchElementException("Called dequeueQ on empty queue.");
+        }
+
+        public T max() {
+            if (!candidatesForMax.isEmpty()) {
+                return candidatesForMax.getFirst();
+            }
+            throw new NoSuchElementException("empty queue");
+        }
+    }
+}
