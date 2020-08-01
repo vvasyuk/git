d00475815855d178f18b68dfa604d4b650934e04
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2020-08-02 00:37:31 +0300

    upd

---
+++ perl/test.pl
@@ -0,0 +1,13 @@
+print "hi\n";
+#my $name = $ARGV[0];
+#print $name;
+
+foreach my $arg (@ARGV) {
+    print $arg, "\n";
+}
+
+#my @teams = ('cubs', 'reds', 'yankees', 'dodgers');
+
+#foreach (@ARGS) {
+#  print "$_\n";
+#}
---
+++ perl/vsCodeBoxWrap.pl
@@ -0,0 +1,16 @@
+my @lines = split /\n/, @ARGV[0];
+#print @lines[0];
+
+my $max = -1;
+for my $line (@lines) {
+    if (length($line) > $max) {
+        $max = length($line);
+    }
+}
+
+print "#" x ($max+4) . "\n";
+for my $s (@lines) {
+    chomp $s;
+    print "# " . $s  . " " x ($max-length($s)) . " #\n";
+}
+print "#" x ($max+4) . "\n";
