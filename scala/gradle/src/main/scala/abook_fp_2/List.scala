d1e6d8a9b49f6df5bf3af0b48b305882324d5fad
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2020-11-12 23:38:01 +0200

    upd

---
+++ scala/gradle/src/main/scala/abook_fp_2/List.scala
@@ -0,0 +1,67 @@
+package abook_fp_2
+
+sealed trait myList[+A]{
+  def isEmpty: Boolean
+  def head: A
+  def tail: myList[A]
+}
+case object Nil extends myList[Nothing] {
+  override def isEmpty: Boolean = true
+  override def head: Nothing = throw new Exception
+  override def tail: myList[Nothing] = throw new Exception
+}
+case class Cons[+A](head: A, tail: myList[A]) extends myList[A] {
+  override def isEmpty: Boolean = false
+}
+
+object myList {
+  def sum(ints: myList[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x,xs) => x + sum(xs)
+  }
+  def product(ds: myList[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x,xs) => x * product(xs)
+  }
+
+  def tail[A](l: myList[A]): myList[A] = l match {
+      case Nil => sys.error("tail of empty list")
+      case Cons(_,t) => t
+  }
+
+  def setHeader[A](l: myList[A], header: A): myList[A] = l match {
+    case Nil => Cons(header, Nil)
+    case Cons(_,t) => Cons(header, t)
+  }
+
+  def drop[A](l: myList[A], n: Int): myList[A] = {
+    if (n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_,t) => drop(t, n-1)
+    }
+  }
+
+  def dropWhile[A](l: myList[A], f: A => Boolean): myList[A] = l match {
+    case Cons(h,t) if f(h) => dropWhile(t, f)
+    case _ => l
+  }
+
+  def append[A](a1: myList[A], a2: myList[A]): myList[A] = a1 match {
+      case Nil => a2
+      case Cons(h,t) => Cons(h, append(t, a2))
+  }
+
+  def init[A](l: myList[A]): myList[A] =   l match {
+    case Nil => sys.error("init of empty list")
+    case Cons(_,Nil) => Nil
+    case Cons(h,t) => Cons(h,init(t))
+  }
+
+  def apply[A](as: A*): myList[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+
+}
---
+++ scala/sbt/sparkTest/build.sbt
@@ -0,0 +1,24 @@
+name := "sparkTest"
+
+version := "0.1"
+
+scalaVersion := "2.12.11"
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.4.5",
+  "org.apache.spark" %% "spark-sql" % "2.4.5",
+//  "org.apache.spark" %% "spark-core" % "2.3.3",
+//  "org.apache.spark" %% "spark-sql" % "2.3.3",
+  "com.jsuereth" %% "scala-arm" % "2.0",
+  "org.scalatest" %% "scalatest" % "3.0.8" % Test,
+  "org.scalamock" %% "scalamock" % "4.4.0" % Test,
+  "com.h2database" % "h2" % "1.4.200" % Test
+)
+
+// Do not include Scala in the assembled JAR
+//assemblyOption in assembly := (assemblyOption in assembly).value.copy(includeScala = false)
+//
+//// META-INF discarding for the FAT JAR
+//assemblyMergeStrategy in assembly := {
+//  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+//  case x => MergeStrategy.first
+//}
