5f50205ea726f18283dd17372c02c5f319ffca55
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-01-04 00:13:08 +0200

    upd

--- coursera/aws/lambda.py
+++ coursera/aws/lambda.py
@@ -3,17 +3,11 @@ from wdap.client import WDAPClient
 import pandas as pd
 from io import BytesIO
 from time import gmtime, strftime
+import pyarrow.parquet as pq
+import awswrangler as wr
 
 print('Loading function')
 
-s3 = boto3.client("s3")
-
 def lambda_handler(event, context):
-    #wdap = WDAPClient(login='vadym.vasiuk@infopulse.com', password='oiGRI6vLlFE52hj2vwAa')
-    #result = wdap.get_users(groups=True, return_response=False)
-    buffer = BytesIO()
-    object = s3.Object('glue-demo-bucket-vasiuk', 'master/datapoint/*')
-    object.download_fileobj(buffer)
-    
-    df = pd.read_parquet(buffer)
+    df = wr.s3.read_parquet(path='s3://glue-demo-bucket-vasiuk/transform/partition_dt=20210102/')
     print(df.to_string)
