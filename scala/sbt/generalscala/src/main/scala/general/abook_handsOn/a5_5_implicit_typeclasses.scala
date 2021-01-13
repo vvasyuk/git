04ed8ce4996259bbcd6df2b34b405f1c35955a10
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-01-14 00:36:01 +0200

    upd

--- scala/sbt/generalscala/src/main/scala/general/abook_handsOn/a5_5_implicit_typeclasses.scala
+++ scala/sbt/generalscala/src/main/scala/general/abook_handsOn/a5_5_implicit_typeclasses.scala
@@ -1,40 +1,40 @@
-package general.abook_handsOn
-
-trait StrParser[T]{ def parse(s: String): T }
-object StrParser{
-  implicit object ParseInt extends StrParser[Int]{
-    def parse(s: String) = s.toInt
-  }
-  implicit object ParseBoolean extends StrParser[Boolean]{
-    def parse(s: String) = s.toBoolean
-  }
-  implicit object ParseDouble extends StrParser[Double]{
-    def parse(s: String) = s.toDouble
-  }
-}
-
-object a5_5_implicit_typeclasses {
-  def parseFromString[T](s: String)(implicit parser: StrParser[T]) = {
-    parser.parse(s)
-  }
-  def parseFromConsole[T](implicit parser: StrParser[T]) = {
-    parser.parse(scala.Console.in.readLine())
-  }
-  implicit def ParseSeq[T](implicit p: StrParser[T]) = new StrParser[Seq[T]]{
-    def parse(s: String) = s.split(',').toSeq.map(p.parse)
-  }
-
-  def main(args: Array[String]): Unit = {
-
-    val args = Seq("123", "true", "7.5")
-    val myInt = parseFromString[Int](args(0))
-    val myBoolean = parseFromString[Boolean](args(1))
-    val myDouble = parseFromString[Double](args(2))
-
-    println(s"$myInt $myBoolean $myDouble")
-
-    val seqBooleans = parseFromString[Seq[Boolean]]("true,false,true")
-    println(s"$seqBooleans")
-  }
-
-}
+//package general.abook_handsOn
+//
+//trait StrParser[T]{ def parse(s: String): T }
+//object StrParser{
+//  implicit object ParseInt extends StrParser[Int]{
+//    def parse(s: String) = s.toInt
+//  }
+//  implicit object ParseBoolean extends StrParser[Boolean]{
+//    def parse(s: String) = s.toBoolean
+//  }
+//  implicit object ParseDouble extends StrParser[Double]{
+//    def parse(s: String) = s.toDouble
+//  }
+//}
+//
+//object a5_5_implicit_typeclasses {
+//  def parseFromString[T](s: String)(implicit parser: StrParser[T]) = {
+//    parser.parse(s)
+//  }
+//  def parseFromConsole[T](implicit parser: StrParser[T]) = {
+//    parser.parse(scala.Console.in.readLine())
+//  }
+//  implicit def ParseSeq[T](implicit p: StrParser[T]) = new StrParser[Seq[T]]{
+//    def parse(s: String) = s.split(',').toSeq.map(p.parse)
+//  }
+//
+//  def main(args: Array[String]): Unit = {
+//
+//    val args = Seq("123", "true", "7.5")
+//    val myInt = parseFromString[Int](args(0))
+//    val myBoolean = parseFromString[Boolean](args(1))
+//    val myDouble = parseFromString[Double](args(2))
+//
+//    println(s"$myInt $myBoolean $myDouble")
+//
+//    val seqBooleans = parseFromString[Seq[Boolean]]("true,false,true")
+//    println(s"$seqBooleans")
+//  }
+//
+//}
---
+++ scala/sbt/generalscala/src/main/scala/general/abook_handsOn/a5_6_exsz_3_typeclass.scala
@@ -0,0 +1,74 @@
+package general.abook_handsOn
+
+trait StrParser[T]{ def parse(s: String): T }
+object StrParser{
+  implicit object ParseInt extends StrParser[Int]{
+    def parse(s: String) = s.toInt
+  }
+  implicit object ParseBoolean extends StrParser[Boolean]{
+    def parse(s: String) = s.toBoolean
+  }
+  implicit object ParseDouble extends StrParser[Double]{
+    def parse(s: String) = s.toDouble
+  }
+  implicit def ParseSeq[T](implicit p: StrParser[T]) = new StrParser[Seq[T]]{
+    def parse(s: String) = splitExpressions(s).map(p.parse)
+  }
+
+  implicit def ParseTuple[T, V](implicit p1: StrParser[T], p2: StrParser[V]) =
+    new StrParser[(T, V)]{
+      def parse(s: String) = {
+        val Seq(left, right) = splitExpressions(s)
+        (p1.parse(left), p2.parse(right))
+      }
+    }
+
+  def splitExpressions(s: String): Seq[String] = {
+    assert(s.head == '[')
+    assert(s.last == ']')
+    val indices = collection.mutable.ArrayDeque.empty[Int]
+    var openBrackets = 0
+    for(i <- Range(1, s.length - 1)){
+      s(i) match{
+        case '[' => openBrackets += 1
+        case ']' => openBrackets -= 1
+        case ',' =>
+          if (openBrackets == 0) indices += i
+        case _ => // do nothing
+      }
+    }
+    val allIndices = Seq(0) ++ indices ++ Seq(s.length - 1)
+    for(i <- Range(1, allIndices.length).toList)
+      yield s.substring(allIndices(i - 1) + 1, allIndices(i))
+  }
+}
+
+object a5_6_exsz_3_typeclass {
+  def parseFromString[T](s: String)(implicit parser: StrParser[T]) = {
+    parser.parse(s)
+  }
+//  implicit def ParseSeq[T](implicit p: StrParser[T]) = new StrParser[Seq[T]]{
+//    def parse(s: String) = s.split(',').toSeq.map(p.parse)
+//  }
+
+  def main(args: Array[String]): Unit = {
+    val args = Seq("123", "true", "7.5")
+    val myInt = parseFromString[Int](args(0))
+    val myBoolean = parseFromString[Boolean](args(1))
+    val myDouble = parseFromString[Double](args(2))
+//    val parseSeq = parseFromString[Seq[Boolean]]("true,false,true")
+    val parseSeq2 = parseFromString[Seq[Boolean]]("[true,false,true]")
+
+    val parseSeq3 = parseFromString[Seq[(Seq[Int], Seq[Boolean])]]("[" +
+      "[[1],[true]]," +
+      "[[2,3],[false,true]]," +
+      "[[4,5,6],[false,true,false]]" +
+      "]")
+
+    println(parseSeq3)
+
+    println(s"$myInt $myBoolean $myDouble ${parseSeq2.mkString(",")}")
+  }
+
+}
+
