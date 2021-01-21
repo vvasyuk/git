01b9780fb69dcf281919994811c62298e7a3d0ad
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-01-21 23:21:13 +0200

    upd

---
+++ scala/sbt/generalscala/src/main/scala/general/abook_handsOn/a6_10_exsz_5_trie.scala
@@ -0,0 +1,56 @@
+package general.abook_handsOn
+
+class Trie[T]() {
+  class Node(var value: Option[T],
+             val children: collection.mutable.Map[Char, Node] = collection.mutable.Map())
+  val root = new Node(None)
+  def add(s: String, v: T) = {
+    var current = root
+    for (c <- s) current = current.children.getOrElseUpdate(c, new Node(None))
+    current.value = Some(v)
+  }
+  def contains(s: String): Boolean = get(s).isDefined
+  def get(s: String): Option[T] = {
+    var current = Option(root)
+    for (c <- s if current.nonEmpty) current = current.get.children.get(c)
+    current.flatMap(_.value)
+  }
+  def prefixesMatchingString0(s: String): Map[Int, T] = {
+    var current = Option(root)
+    val output = Map.newBuilder[Int, T]
+    for ((c, i) <- s.zipWithIndex if current.nonEmpty) {
+      for (v <- current.get.value) output += (i -> v)
+      current = current.get.children.get(c)
+    }
+    for (c <- current; v <- c.value) output += (s.length -> v)
+    output.result()
+  }
+  def prefixesMatchingString(s: String): Map[String, T] = {
+    prefixesMatchingString0(s).map { case (k, v) => (s.substring(0, k), v)}
+  }
+  def stringsMatchingPrefix(s: String): Map[String, T] = {
+    var current = Option(root)
+    for (c <- s if current.nonEmpty) current = current.get.children.get(c) // initial walk
+    if (current.isEmpty) Map()
+    else {
+      val output = Map.newBuilder[String, T]
+      def recurse(current: Node, path: List[Char]): Unit = {
+        for (v <- current.value) output += (s + path.reverse.mkString -> v)
+        for ((c, n) <- current.children) recurse(n, c :: path)
+      }
+      recurse(current.get, Nil) // recursive walk
+      output.result()
+    }
+  }
+}
+
+object a6_10_exsz_5_trie {
+  def main(args: Array[String]): Unit = {
+    val t = new Trie[Int]()
+    t.add("mango", 1337)
+    t.add("mandarin", 31337)
+    t.add("map", 37)
+    t.add("man", 7)
+  }
+}
+
