93ba870ac32968fddb4d9ab254096f2622d4e97e
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-01-26 20:38:05 +0200

    upd

--- scala/sbt/generalscala/src/main/scala/general/abasics/collections/mapMutableMap.scala
+++ scala/sbt/generalscala/src/main/scala/general/abasics/collections/mapMutableMap.scala
@@ -1,5 +1,22 @@
 package general.abasics.collections
 
-object mapMutableMap {
+import scala.collection.mutable.Map
 
+object mapMutableMap {
+  def main(args: Array[String]): Unit = {
+    // Builders
+    val m1 = Map(1->"one", 2->"two")
+
+    // modify
+    m1(3) = "three"; assert(m1 == Map(1->"one", 2->"two",3->"three")); m1 -= 3
+    m1 += (3->"three"); assert(m1 == Map(1->"one", 2->"two",3->"three"))
+    m1 -= (3); assert(m1 == Map(1->"one", 2->"two"))
+    m1 ++=Map(3->"three"); assert(m1 == Map(1->"one", 2->"two",3->"three"))
+    m1 --=List(3); assert(m1 == Map(1->"one", 2->"two"))
+
+    assert(m1.getOrElseUpdate(2,"two") == "two")
+    assert(m1.getOrElseUpdate(3,"three") == "three")
+    assert(m1 == Map(1->"one", 2->"two",3->"three"))
+    m1-=3
+  }
 }
