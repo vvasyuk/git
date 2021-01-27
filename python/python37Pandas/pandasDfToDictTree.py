68dcc4e8cf0336b571d030c73ac66ce2c3039536
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-01-27 20:46:57 +0200

    upd

--- python/python37Pandas/pandasDfToDictTree.py
+++ python/python37Pandas/pandasDfToDictTree.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import math
 
 # def make_map(list_child_parent):
 #     has_parent = set()
@@ -35,44 +36,65 @@ import pandas as pd
 #     print(result)
 #     return result
 
-data0 = [
-    [1, ''],
-    [2, 1],
-    [3, 2]
-]
-data = [
-    [13, '', '001'],
-    [2, 13, ''],
-    [3, 13, ''],
-    [4, 2, '']
-]
-df = pd.DataFrame(data, columns = ['id', 'parent', 'ext_id'])
+# data0 = [
+#     [1, ''],
+#     [2, 1],
+#     [3, 2]
+# ]
+# data = [
+#     [13, '', '001'],
+#     [2, 13, ''],
+#     [3, 13, ''],
+#     [4, 2, '']
+# ]
+# df = pd.DataFrame(data, columns = ['id', 'parent', 'ext_id'])
+df = pd.read_csv("c:/Users/jopa/Downloads/data/testData/levels/datapoint/master.csv")
+print(df.to_string())
 
 class Node(object):
-    def __init__(self, v, has_parent=False):
-        self.value = v
+    def __init__(self, id, wap_id='', name='', currency='', shortName='', has_parent=False):
+        self.id = id
+        self.wap_id = wap_id
+        self.name = name
+        self.currency = currency
+        self.shortName = shortName
         self.has_parent = has_parent
         self.childs = []
 
-    def traverse(self, lvl):
-        print(" "*lvl + str(self.value))
+    def traverse(self, lvl, result):
+        print(" "*lvl + str(self.id))
+        tmp_result = {'id': self.wap_id,'name': self.name,'currency': self.currency,'shortName': self.shortName, 'children': []}
+        result.append(tmp_result)
         for c in self.childs:
-            c.traverse(lvl+1)
+            c.traverse(lvl+1, tmp_result['children'])
 
 def make_map(df):
     root = Node(0)
     all_items = {}
     for index, row in df.iterrows():
-        if row['parent'] not in all_items:
-            all_items[row['parent']] = Node(row['parent'])
-        if row['id'] not in all_items:
-            all_items[row['id']] = Node(row['id'], True)
-        all_items[row['parent']].childs.append(all_items[row['id']])
-
-    for key, value in all_items.items():
-        if not value.has_parent:
-            root.childs.append(value)
+        id = row['proj_id']
+        parent = row['prnt_proj_id']
+        if parent not in all_items:
+            all_items[parent] = Node(parent)
+            if isinstance(parent, float) and math.isnan(parent):
+                root.childs.append(all_items[parent])
+        if id not in all_items:
+            wap_id_raw = row['wap_id']
+            wap_id = '' if isinstance(wap_id_raw, float) and math.isnan(wap_id_raw) else wap_id_raw
+            all_items[id] = Node(id, wap_id, str(row['proj_name']), 'USD', '',  True)
+        all_items[parent].name = row['proj_name']
+        all_items[parent].currency = 'USD'
+        all_items[parent].childs.append(all_items[id])
     return root
 
 res = make_map(df)
-res.traverse(1)
+result = []
+res.traverse(1, result)
+
+print(result[0]['children'][0]['children'])
+
+# result = []
+# result.append({'proj_name': 'name1', 'children': []})
+# child = result[0]['children']
+# child.append({'proj_name': 'name11', 'children': []})
+# print(result)
--- scala/sbt/generalscala/src/main/scala/general/abasics/collections/setTest.scala
+++ scala/sbt/generalscala/src/main/scala/general/abasics/collections/setTest.scala
@@ -1,5 +1,20 @@
 package general.abasics.collections
 
 object setTest {
+  def main(args: Array[String]): Unit = {
+    // Builders
+    val s1 = Set(1,2)
 
+    // modify
+    assert(s1 + 3 == Set(1,2,3))
+    assert(s1 - 2 == Set(1))
+    assert(s1 ++ Set(3) == Set(1,2,3))
+    assert(s1 -- Set(2) == Set(1))
+
+    // set specific
+    val s2 = Set(2,3)
+    assert(s1.intersect(s2) == Set(2))
+    assert(s1.diff(s2) == Set(1))
+    assert(s1.subsetOf(Set(1,2,3)) == true)
+  }
 }
