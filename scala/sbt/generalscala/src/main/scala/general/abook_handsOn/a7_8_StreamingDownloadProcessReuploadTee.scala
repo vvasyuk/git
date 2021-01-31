adaa10dd8a903fbad6d52fa3f1c2e608779b0cf9
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-01-31 21:48:48 +0200

    upd

---
+++ scala/sbt/generalscala/src/main/scala/general/abook_handsOn/a7_8_StreamingDownloadProcessReuploadTee.scala
@@ -0,0 +1,16 @@
+package general.abook_handsOn
+
+object a7_8_StreamingDownloadProcessReuploadTee {
+  def main(args: Array[String]): Unit = {
+    val download = os.proc( "curl", "https://api.github.com/repos/lihaoyi/mill/releases").spawn()
+    val base64 = os.proc("base64").spawn(stdin = download.stdout)
+    val gzip = os.proc("gzip").spawn(stdin = base64.stdout)
+    val tee = os.proc("tee", "base64.gz").spawn(stdin = gzip.stdout)
+    val upload = os
+      .proc("curl", "-X", "PUT", "-d", "@-", "https://httpbin.org/anything")
+      .spawn(stdin = tee.stdout)
+
+    val contentLength = upload.stdout.lines.filter(_.contains("Content-Length"))
+    println(contentLength)
+  }
+}
