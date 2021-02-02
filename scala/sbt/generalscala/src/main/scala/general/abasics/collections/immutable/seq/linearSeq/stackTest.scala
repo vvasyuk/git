bf5bd4b0c086fcf5fee856690fe01249a2e422c7
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-02-02 21:14:38 +0200

    upd

---
+++ scala/sbt/generalscala/src/main/scala/general/abasics/collections/immutable/seq/linearSeq/stackTest.scala
@@ -0,0 +1,9 @@
+package general.abasics.collections.immutable.seq.linearSeq
+
+// deprecated since 2.13
+// recommended to use list to achieve same functionality
+object stackTest {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
