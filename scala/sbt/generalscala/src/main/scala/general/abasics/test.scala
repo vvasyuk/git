0ec99f34fb1d60f1117c3e6dae79b0c2ac22b1a1
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-02-14 15:33:10 +0200

    upd

--- scala/sbt/generalscala/src/main/scala/general/abasics/test.scala
+++ scala/sbt/generalscala/src/main/scala/general/abasics/test.scala
@@ -1,19 +1,16 @@
 package general.abasics
 
 object test {
-  class TipCalculator {
-    public List<String> addPerson(List<String> names,
-      String name) {
-      List<String> updated = new ArrayList<>(names);
-      updated.add(name);
-      return updated;
-    }
-    public static int getTipPercentage(List<String> names) {
-      if(names.size() > 5) {
-        return 20;
-      } else if(names.size() > 0) {
-        return 10;
-      } else return 0;
-    }
+  private List<String> plan = new ArrayList<>();
+  public void replan(String newCity, String beforeCity) {
+    int newCityIndex = plan.indexOf(beforeCity);
+    plan.add(newCityIndex, newCity);
+  }
+  public void add(String city) {
+    plan.add(city);
+  }
+  public List<String> getPlan() {
+    return Collections.unmodifiableList(plan);
   }
 }
+
