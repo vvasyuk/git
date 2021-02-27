cdcf12518cba36ad8b291b1b9bc4e666c8379ad5
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-02-27 03:23:52 +0200

    upd

--- python/python37Pandas/sparkTest/sparkDFOverwritePartition.py
+++ python/python37Pandas/sparkTest/sparkDFOverwritePartition.py
@@ -1,19 +1,26 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 import time
+import os
+print(os.environ['HADOOP_HOME'])
+print(os.environ['JAVA_HOME'])
+print(os.environ['SPARK_HOME'])
 
-spark = SparkSession.builder.config("spark.sql.warehouse.dir", "file:///C:/temp").appName("PopularMovies").getOrCreate()
+spark = SparkSession.builder\
+    .config("spark.sql.warehouse.dir", "file:///C:/temp")\
+    .appName("PopularMovies").getOrCreate()
 #spark.conf.set("spark.sql.sources.partitionOverwriteMode", "dynamic")
-spark.conf.set("spark.sql.parquet.compression.codec", "gzip")
+#spark.conf.set("spark.sql.parquet.compression.codec", "gzip")
+# spark._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")
 df = spark.createDataFrame(
     [
         (0, 'name0', 10, 20, 20210226),
     ],
     ['id', 'name', 'col1', 'col2', 'partition_dt']
 )
-#df.show(10,False)
+df.show(10,False)
 
-df.write.parquet("C:\\work\\tryout\\python\\python37Pandas\\sparkTest\\result")
+df.write.parquet("file:///C:/work/tryout/python/python37Pandas/sparkTest/result")
 # .partitionBy("partition_dt")
 # .mode("overwrite")
 # .parquet(“s3://spark-output”))
