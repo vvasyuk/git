2ef0b6c8e09442aa701cc38f213e966f89fc9437
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-03-10 01:31:43 +0200

    upd

--- python/python37Pandas/wdapTest/getLevels.py
+++ python/python37Pandas/wdapTest/getLevels.py
@@ -1,6 +1,7 @@
 from wdapTest.wdap.client import WDAPClient
 import os
 
+#wdap = WDAPClient(login='Viacheslav.Onufriien@infopulse.com', password='148Eighth')
 wdap = WDAPClient(login=str(os.environ['login']), password=str(os.environ['password']))
 levels = wdap.get_levels()
 if levels['success']:
