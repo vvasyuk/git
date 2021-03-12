59bbbb5e1f385e48e6519407c4ed102fc80e2fe3
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-03-13 01:38:11 +0200

    upd

--- python/python37Pandas/general/jsonTest.py
+++ python/python37Pandas/general/jsonTest.py
@@ -1,6 +1,18 @@
 import json
 
-myStr = '{"vadym.vasiuk@deloitte2.com": "oiGRI6vLlFE52hj2vwAa"}'
-loginPswd = [(k, v) for k, v in json.loads(myStr).items()]
-print(loginPswd[0][0])
-print(loginPswd[0][1])
+# def myMethod(s):
+#     print(s)
+#     #myStr =
+#     loginPswd = [(k, v) for k, v in json.loads(s).items()]
+#     print(loginPswd[0][0])
+#     print(loginPswd[0][1])
+#
+# myMethod('{"vadym.vasiuk@deloitte2.com": "oiGRI6vLlFE52hj2vwAa"}')
+
+input = {'a': """{"s3_bucket": "wdap-bucket","date": "20210312"}"""}
+v = input['a']
+print(v)
+# s1 = '{\"s3_bucket\": \"wdap-bucket\",\"date\": \"20210312\"}'
+d = json.loads(v)
+date = d['date']
+# print(date)
--- python/python37Pandas/sparkTest/sparkLevels.py
+++ python/python37Pandas/sparkTest/sparkLevels.py
@@ -5,8 +5,14 @@ import time
 spark = SparkSession.builder.config("spark.sql.warehouse.dir", "file:///C:/temp").appName("PopularMovies").getOrCreate()
 
 # raw
-# dfRaw = spark.read.parquet("c:\\work\\project\\data\\raw\\datapoint\\levels\\part-00000-b61b6840-c775-45f0-ba00-dd56923a08dd.c000.snappy.parquet")
-# dfRaw.show(20,False)
+# dfRawDp = spark.read.parquet("c:\\work\\project\\data\\testData\\levels\\datapoint\\raw")
+# dfRawDp.show(20,False)
+#.select(col('proj_id'))\
+
+
+# raw wdap
+dfRawWp = spark.read.parquet("c:\\work\\project\\data\\raw\\wdap\\levels\\latest\\")
+dfRawWp.show(20,False)
 
 # wdap
 # dfRaw = spark.read.parquet("c:\\work\\project\\data\\raw\\wdap\\levels\\latest\\raw_datapoint_levels")
@@ -16,5 +22,5 @@ spark = SparkSession.builder.config("spark.sql.warehouse.dir", "file:///C:/temp"
 # dfRaw.show(20,False)
 
 # master
-dfRaw = spark.read.parquet("c:\\work\\project\\data\\master\\datapoint\\levels\\latest\\part-00000-d1fb2fb9-d4ed-4fa8-ac9a-fc5f08f64646-c000.snappy.parquet")
-dfRaw.filter(col('proj_id').like("%TWVA00001%")).show(20,False)
+# dfRaw = spark.read.parquet("c:\\work\\project\\data\\master\\datapoint\\levels\\latest\\part-00000-d1fb2fb9-d4ed-4fa8-ac9a-fc5f08f64646-c000.snappy.parquet")
+# dfRaw.filter(col('proj_id').like("%TWVA00001%")).show(20,False)
