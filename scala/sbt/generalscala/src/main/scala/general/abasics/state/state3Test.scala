4547327b21107f964fb2c3b2174610ae77fd03aa
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-04-04 01:06:45 +0300

    upd

--- scala/sbt/generalscala/src/main/scala/general/abasics/state/state3Test.scala
+++ scala/sbt/generalscala/src/main/scala/general/abasics/state/state3Test.scala
@@ -2,23 +2,33 @@ package general.abasics.state
 
 object state3Test {
   def main(args: Array[String]): Unit = {
-    val stateWithNewDistance: State[GolfState, Int] = for {
-      _ <- swing(20)
-      _ <- swing(15)
-      totalDistance <- swing(0)
-    } yield totalDistance
+//    val stateWithNewDistance: State[GolfState, Int] = for {
+//      _ <- swing(20)
+//      _ <- swing(15)
+//      totalDistance <- swing(0)
+//    } yield totalDistance
+//
+//    val beginningState = GolfState(0)
+//    val result: (GolfState, Int) = stateWithNewDistance.run(beginningState)
+//
+//    println(s"GolfState: ${result._1}") //GolfState(35)
+//    println(s"Total Distance: ${result._2}") //35
 
-    val beginningState = GolfState(0)
-    val result: (GolfState, Int) = stateWithNewDistance.run(beginningState)
+//    val a = swing(10)
+//    val res1 = a.run(GolfState(0))
+//    println(res1)
 
-    println(s"GolfState: ${result._1}") //GolfState(35)
-    println(s"Total Distance: ${result._2}") //35
+    val s2 = swing(10).flatMap(_ => swing(10))
+    val res2 = s2.run(GolfState(0))
+    println(res2)
+
   }
 
   case class State[S, A](run: S => (S, A)) {
     def flatMap[B](g: A => State[S, B]): State[S, B] = State { (s0: S) =>
       val (s1, a) = run(s0)
-      g(a).run(s1)
+      val res = g(a).run(s1)
+      res
     }
     def map[B](f: A => B): State[S, B] = flatMap(a => State.point(f(a)))
   }
