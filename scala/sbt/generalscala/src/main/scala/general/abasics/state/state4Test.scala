e83e814a5145f1415c1eebe771f2cea3577c6158
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-04-04 01:06:55 +0300

    upd

---
+++ scala/sbt/generalscala/src/main/scala/general/abasics/state/state4Test.scala
@@ -0,0 +1,34 @@
+package general.abasics.state
+
+object state4Test {
+
+  def main(args: Array[String]): Unit = {
+    val initState = GolfState(0)
+
+    val r1 = plus(2)
+    val res1 = r1.run(initState)
+    println(res1)
+
+    val x = (_: Int) => plus(2)
+    val res2 = x(13).run(initState)
+    println(res2)
+  }
+
+  case class State[S, A](run: S => (S, A)) {
+    def flatMap[B](g: A => State[S, B]): State[S, B] = State { (s0: S) =>
+      val (s1, a) = run(s0)
+      val res = g(a).run(s1)
+      res
+    }
+    def map[B](f: A => B): State[S, B] = flatMap(a => State.point(f(a)))
+  }
+  object State {
+    def point[S, A](v: A): State[S, A] = State(run = s => (s, v))
+  }
+
+  case class GolfState(amnt: Int)
+  def plus(amnt: Int): State[GolfState,Int] = State{ (s:GolfState) =>
+    val newAmnt = s.amnt + amnt
+    (GolfState(newAmnt), newAmnt)
+  }
+}
