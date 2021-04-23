a92026d6b47be984fd43de4a32af446b1c493a4c
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-04-23 23:02:28 +0300

    upd

---
+++ scala/sbt/generalscala/src/main/scala/general/abasics/partialFunction/partial1Test.scala
@@ -0,0 +1,33 @@
+package general.abasics.partialFunction
+
+// a function defined as (Int) => String takes any Int and returns a String.
+// a Partial Function is only defined for certain values of the defined type. A Partial Function (Int) => String might not accept every Int.
+object partial1Test {
+
+  case class PhoneExt(name: String, ext: Int)
+
+  def main(args: Array[String]): Unit = {
+    val one: PartialFunction[Int, String] = { case 1 => "one" }
+
+    println(one(1))
+    assert(one.isDefinedAt(1) == true)
+    assert(one.isDefinedAt(2) == false)
+
+    // composition
+
+    val two: PartialFunction[Int,String] = {case 2 => "two"}
+    val three: PartialFunction[Int,String] = {case 3 => "three"}
+    val sElse: PartialFunction[Int,String] = {case _ => "something else"}
+
+    val partial = two orElse three orElse sElse
+    assert(partial(5)=="something else")
+    assert(partial(3)=="three")
+    assert(partial(2)=="two")
+
+    // magic
+
+    val extensions = List(PhoneExt("steve", 100), PhoneExt("robey", 200))   // filter takes a function. In this case a predicate function of (PhoneExt) => Boolean.
+    val res = extensions.filter{ case PhoneExt(a,b) => b < 200 }            //A PartialFunction is a subtype of Function so filter can also take a PartialFunction!
+    println(res)
+  }
+}
