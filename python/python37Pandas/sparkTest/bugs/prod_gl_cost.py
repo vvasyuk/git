d0a24a99afb4a2d20825c0f2a96824a84e09ee1f
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-05-14 12:35:56 +0300

    upd

---
+++ python/python37Pandas/sparkTest/bugs/prod_gl_cost.py
@@ -0,0 +1,10 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+import time
+
+spark = SparkSession.builder.config("spark.sql.warehouse.dir", "file:///C:/temp").appName("ImportantStuff").getOrCreate()
+
+# transform
+dfTransform = spark.read.parquet("C:\\work\\project\\bugs\\prod_11_gl_cost").cache()
+print(dfTransform.count())
+dfTransform.show(10000,False)
