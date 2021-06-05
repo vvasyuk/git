ea72dab31619746572eecc0e9f075289c3e3bb12
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-06-06 01:39:54 +0300

    upd

--- scala/sbt/scala3cross/src/main/scala/dcp/q001_twoSum.scala
+++ scala/sbt/scala3cross/src/main/scala/dcp/q001_twoSum.scala
@@ -1,19 +1,41 @@
 package dcp
+import scala.collection.mutable.HashSet
 
 object q001_twoSum {
   def main(args: Array[String]): Unit = {
-    val arr = Array(10, 15, 3, 7)
+    val arr = Array(10, 15, 3, 7, 1)
     val k = 17
 
     sumTwo(arr, 17)
+
+    sumTwo2(arr, 17)
   }
 
-  def sumTwo(arr: Array[Int], k: Int): Boolean = {
-    var s = Set[Int]()
-    for (i <- arr)
-      println(i)
+  def sumTwo(arr: Array[Int], k: Int): Boolean =
+    val s = HashSet[Int]()
+    var res = false
+    for (i <- arr if !res)
+      println(s"processing ${i}")
+      if !s(k-i) then
+        println(s"adding ${i} to set")
+        s += i
+        else
+        println(s"found a sum")
+        res = true
+    res
 
-    true
-  }
-
+  def sumTwo2(arr: Array[Int], k: Int): Boolean = 
+    val s = HashSet[Int]()
+    var res = false
+    arr.foldLeft((s,res))((acc, v) => {
+      var (s, r) = acc
+      if !s(k-v) then
+        println(s"adding ${v} to set")
+        s += v
+        else
+        println(s"found a sum")
+        r = true
+      (s,r)
+    })
+    res
 }
