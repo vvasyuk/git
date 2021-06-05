c6f7d823eaca68d9cc3cd45ef6cc0735447f8f93
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-06-06 01:05:53 +0300

    upd

---
+++ scala/sbt/scala3cross/src/main/scala/dcp/q001_twoSum.scala
@@ -0,0 +1,19 @@
+package dcp
+
+object q001_twoSum {
+  def main(args: Array[String]): Unit = {
+    val arr = Array(10, 15, 3, 7)
+    val k = 17
+
+    sumTwo(arr, 17)
+  }
+
+  def sumTwo(arr: Array[Int], k: Int): Boolean = {
+    var s = Set[Int]()
+    for (i <- arr)
+      println(i)
+
+    true
+  }
+
+}
