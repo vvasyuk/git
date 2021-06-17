ec2392eaec85df1b798c7bc5daf414ef6f9092f8
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-06-17 20:50:27 +0300

    upd

--- scala/sbt/scala3cross/src/main/scala/dcp/common/tree/MyTree.scala
+++ scala/sbt/scala3cross/src/main/scala/dcp/common/tree/MyTree.scala
@@ -1,5 +1,50 @@
 package dcp.common.tree
 
-trait MyTree
+import dcp.common.tree.MyTree.treeToArray
+
+trait MyTree:
+  def beautify: Int =
+    val depth = MyTree.depth(this)
+    val arrPads = Array.fill(depth+1)((1,1))
+    for{
+      i <- (1 until depth).reverse
+    } arrPads(i) = (arrPads(i+1)._1*2, arrPads(i+1)._2*2+1)
+    
+    val arr = treeToArray(this)
+    val res = StringBuilder()
+    for{
+      i <- 1 until arr.size
+    } res.append()
+    0
+
 object emptyNode extends MyTree
 case class Node(value: Int, left: MyTree = emptyNode, right: MyTree = emptyNode) extends MyTree
+
+object MyTree:
+  def depth(root: MyTree): Int =
+    def _depthHelp(n: MyTree, level: Int): Int = n match
+        case node: Node => Math.max(_depthHelp(node.left, level + 1), _depthHelp(node.right, level + 1))
+        case emptyNode => level-1
+    
+    _depthHelp(root, 1)
+
+  def treeToArray(root: MyTree): Array[List[Int]] =
+    def _treeToArrayHelp(a: Array[List[Int]], n: MyTree, level: Int): Array[List[Int]] = n match
+      case node: Node => {
+        a(level) = node.value :: a(level)
+        _treeToArrayHelp(a, node.left, level+1)
+        _treeToArrayHelp(a, node.right, level+1)
+        a
+      }
+      case emptyNode => a
+    
+    val depth = MyTree.depth(root)
+    val arr = Array.fill(depth+1)(List[Int]())
+    _treeToArrayHelp(arr, root, 1)
+    for{
+      i <- 1 until arr.size
+    } arr(i) = arr(i).reverse
+    arr
+    
+    
+    
--- scala/sbt/scala3cross/src/main/scala/dcp/q003_serializeTree.scala
+++ scala/sbt/scala3cross/src/main/scala/dcp/q003_serializeTree.scala
@@ -6,7 +6,13 @@ import dcp.common.tree.emptyNode
 
 object q003_serializeTree {
   def main(args: Array[String]): Unit =
-
+// 4*2-1
+// 4*2-1
+//        0
+//    1       2
+//  3   4   5   6
+// 3 3 4 4 5 5 6 6
+  
     val n3 = Node(3)
     val n4 = Node(4)
     val n5 = Node(5)
@@ -14,10 +20,18 @@ object q003_serializeTree {
     val n1 = Node(1, n3, n4)
     val n2 = Node(2, n5, n6)
     val root = Node(0, n1, n2)
-    println(root)
+    println(root.beautify)
+
+    
+
+    
 
   def deserialize(root: MyTree): String =
     val s = StringBuilder("")
     s.toString
 }
 
+// TODO: build Array[List[Int]]
+// TODO: add to string for Node
+// TODO: deserialize to a string with comma separator
+// TODO: serialize string into array and array into tree
