f6726b0afc6cdaa5059aa6b1666160c69ac17d61
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-06-21 14:13:57 +0300

    upd

---
+++ scala/sbt/scala3cross/src/main/scala/dcp/q005_pairFirstLast.scala
@@ -0,0 +1,14 @@
+package dcp
+
+// cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair.
+// For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+object q005_pairFirstLast:
+  def main(args: Array[String]): Unit =
+    val pair = MyPair(3,4)
+    assert(pair.car == 3)
+    assert(pair.cdr == 4)
+
+  case class MyPair(f: Int, l: Int):
+    def car = f
+    def cdr = l
