e1f9261e8063be947700f52c70a12f4b1af54a34
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-06-23 22:11:22 +0300

    upd

---
+++ scala/sbt/scala3cross/src/main/scala/dcp/q008_univalTree.scala
@@ -0,0 +1,56 @@
+package dcp
+
+import dcp.common.tree.MyTree
+import dcp.common.tree.Node
+import dcp.common.tree.emptyNode
+
+// A unival tree (which stands for "universal value") is a tree where all nodes under it have the same value.
+//
+//Given the root to a binary tree, count the number of unival subtrees.
+
+// For example, the following tree has 5 unival subtrees:
+//   0
+//  / \
+// 1   0
+//    / \
+//   1   0
+//  / \
+// 1   1
+object q008_univalTree {
+  def main(args: Array[String]): Unit = {
+    val n3 = Node(3)
+    val n4 = Node(4)
+    val n5 = Node(1, Node(1), Node(1))
+    val n6 = Node(0)
+    val n1 = Node(1)
+    val n2 = Node(0, n5, n6)
+    val root = Node(0, n1, n2)
+    println(root.beautify)
+    println(univalCount(root))
+  }
+
+  def univalCount(root: MyTree): Int =
+    def _helper(r: MyTree): (Int, Boolean) = r match{
+      case emptyNode => (1, true)
+      case n: Node   => {
+        val (leftCount, leftUnival) = _helper(n.left)
+        val (rightCount, rightUnival) = _helper(n.right)
+        val total = leftCount + rightCount
+
+        val res = if leftUnival && rightUnival then
+          if n.left != emptyNode && n.value != getChildValue(n.left, n=>n.value).get then (total, false)
+          else if n.right != emptyNode && n.value != getChildValue(n.right, n=>n.value).get then (total, false)
+          else (total+1, true)
+        else
+          (total, false)
+        res
+      }
+    }
+
+    _helper(root)._1
+
+  def getChildValue(root: MyTree, f: Node=>Int): Option[Int] = root match{
+    case emptyNode => None
+    case n: Node => Some(f(n))
+  }
+}
