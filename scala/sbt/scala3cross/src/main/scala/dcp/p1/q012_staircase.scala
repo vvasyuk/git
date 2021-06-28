0cf00b3611883074046fb11cae6b5b22c2c65640
vvasyuk <33102427+vvasyuk@users.noreply.github.com>
2021-06-29 00:41:02 +0300

    upd

---
+++ scala/sbt/scala3cross/src/main/scala/dcp/p1/q012_staircase.scala
@@ -0,0 +1,17 @@
+package dcp.p1
+
+// There exists a staircase with N steps, and you can climb up either 1 or 2 steps at a time. Given N, write a function that returns the number of unique ways you can climb the staircase. The order of the steps matters.
+//
+//For example, if N is 4, then there are 5 unique ways:
+//
+//1, 1, 1, 1
+//2, 1, 1
+//1, 2, 1
+//1, 1, 2
+//2, 2
+
+object q012_staircase {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
